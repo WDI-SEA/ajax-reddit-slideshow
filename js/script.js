ef239e5488ec46ecc8f0b58bea723d8badb52cd4
Scott <scott.heron@outlook.com>
2016-01-29 10:10:05 -0800

    Final final

--- js/script.js
+++ js/script.js
@@ -8,18 +8,19 @@ var redditData = [];
 var userSearch = '';
 var filteredData = [];
 var counter = 0;
-var search = 'cats';
+var search = '';
 
 //$('#searchButton').click(function(){
 //  search = $('#search').val();
 //  console.log(search);
 //});
 
-//$('#hit').on('click', function(){
-//  search = $('#search').val();
-//});
-
-$.get('https://www.reddit.com/search.json?q='+search+'+nsfw:no').done(function(data) {
+$('#hit').on('click', function(e){
+  e.preventDefault();
+  search = $('#search').val();
+  console.log(search);
+ if(search != ''){
+  $.get('https://www.reddit.com/search.json?q='+search+'+nsfw:no').done(function(data) {
     redditData = data;
     
     redditData.data.children.filter(function(child){
@@ -34,21 +35,25 @@ $.get('https://www.reddit.com/search.json?q='+search+'+nsfw:no').done(function(d
     });
     console.log(filteredData);
 
-    $('.carousel').carousel({
-        interval: 3000
-    })
+
 
 
     //setInterval(function() {
     for (var i = 0; i < filteredData.length; i++) {
-      if (filteredData[i] === filteredData[0]){
+      /*if (filteredData[i] === filteredData[0]){
         $('#pic').append('<div class="item active"><img src="'+filteredData[i]+'"></div>');
       }
       else{
         $('#pic').append('<div class="item"><img src="'+filteredData[i]+'"></div>');
-      }
+      }*/
+      $('#pic').append('<div class="item"><img src="'+filteredData[i]+'"></div>');
     }
       
+    if ($('#pic').html() != '') {
+    $('.carousel').carousel({
+        interval: 3000
+    })
+    }
 
       //counter++;
       // if (counter > filteredData.length) {
@@ -58,5 +63,9 @@ $.get('https://www.reddit.com/search.json?q='+search+'+nsfw:no').done(function(d
     //}, 1000);
 
   });
+  }
+});
+
+
 
 });
