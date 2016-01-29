722544d2ae67b9c7e640f2a1ad19794715b2e1e4
dnuzum <derek.nuzum@gmail.com>
2016-01-29 11:20:07 -0800

    still working on getting it to work

--- js/script.js
+++ js/script.js
@@ -1,34 +1,49 @@
 $(document).ready(function(){
-	counter = 0;
-	myInterval = 0;
-	$('#searchBtn').on('click', function(e){
- 		e.preventDefault();
- 		var input = $('.search').val();
-				$('.search').val('');
- 	$.get('http://www.reddit.com/search.json', {
- 		q: input
- 		}).done(function(reddit){
- 			var searchResult = reddit.data.children.map(function(item){
- 				if (item.data.preview !== undefined){
- 					if (item.data.preview.images[0] !== undefined){
- 						return item.data.preview.images[0].source.url;
- 				}
- 			}
+	var images = []
+	var interval = -1
+	var index = 0
+
+ 	function getImages(subject) {
+ 		var url = "http://www.reddit.com/search.json?q="+subject+"nsfw:no";
+ 		$.get(url, {
+ 			}).done(function(data){
+ 			 images = data.data.children.map(function(child){
+ 				return child.data;
+ 				}).filter(function(child) {
+ 					return child.preview != undefined;
+ 				}).map(function(child) {
+ 					return child.preview.images[0].source.url
+ 				})
+ 				console.log(images);
  			});
  			var filtered = searchResult.filter(function (filter){
  				return filter !== undefined;
  			});
- 			myInterval = setInterval( function (){
- 				$('.item active').html('').append("'<data src='+filtered[counter]+' Alt: Reddit Picture '+'src='+filtered[counter]>");
- 				counter ++;
- 				counter = counter % filtered.length;
- 			}, 3000);
- 	
-	});
- 	});
-		$('#stop').on('click', function(){
- 		clearInterval(myInterval);
- 		$('.item active').html('');
- 		$('.container').show(1000);
- 	});
+
+	getImages("cat");
+
+	function startSlideshow() {
+		if (interval != -1) {
+			stopSlideshow();
+		}
+	}
+	interval = setInterval(function() {
+		var imageHolder = $(".input-group-button");
+		imageHolder.html("");
+		imageHolder.html("<img data-src="+images[index++]+' alt="Pictures go here!" src='+images[index++]+"/>");
+		index = index % images.length;
+	}, 2000)
+	function stopSlideshow() {
+		clearInterval(interval);
+		index = 0;
+	}
+	
+		$('#searchBtn').on('click', function(e){
+ 		e.preventDefault();
+ 		var input = $('.search').val();
+				$('.search').val('');
+		stopSlideshow();
+		getImages(toSearch);
+	})
+	}
 });
