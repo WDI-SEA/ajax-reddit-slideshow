1d934ad2b32c57666dacbc947f1c6e7a7241c208
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-01-28 22:47:59 -0800

    slideshow

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
 $(document).ready(function(){
 
- 
-   $.get('https://www.reddit.com/search.json', {
-	  q: 'images'
-	}).done(function(data) {
-	  console.log(data);
-	});
-
-  
+	var reddit= [];
+	var counter = 0;
+	var filteredData = [];
+	
 
 
-});
+		 $("form").on('submit', function(e){
+		 	e.preventDefault();
+		 	var search = $('input').val();
+
+		 
+
+
+	    $.get('https://www.reddit.com/search.json',{q: search }).done(function(data) {
+				reddit = data;
+				$('input').val(' ');
+
+			reddit.data.children.filter(function(child){
+				
+				if(child.data.preview){
+					filteredData.push(child.data.preview.images[0].source.url)
+				}
+				
+			});
+		
+		
+		setInterval(function(){	
+
+			$('#container')
+			.html(' ')
+			.append('<img src=' + filteredData[counter] +'>');
+
+				counter++;
+				if(counter == filteredData.length){
+				  	counter = 0;
+				}
+			}, 2000);
+
+
+		});
+		
+		
+	});		
+});  
