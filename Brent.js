3eef4e56ff724c54dea95af65d189f49284b278c
brytree <brycebortree@gmail.com>
2016-03-31 08:47:09 -0700

    post-working w/ Brent's code & debugging

---
+++ Brent.js
@@ -0,0 +1,167 @@
+// #Reddit photo slideshow
+
+// We are going to use what we've learned so far to create a basic slideshow using images taken from reddit via AJAX.
+
+// ####Content Warning
+
+// Reddit sometimes contains some offensive images be careful with your search terms.
+
+// If you want to ensure that you do not get NSFW (Not Safe For Work) items. You can filter it by adding "nsfw:no" to the end of the search query.
+
+// **Example**
+
+// [http://www.reddit.com/search.json?q=cats nsfw:no](http://www.reddit.com/search.json?q=cats+nsfw:no)
+
+
+
+// ##User Experience
+
+// **Page should load with**
+
+// * Some sort of title
+// * A short description telling the user what to do
+// * A blank text field
+// * A Button ("start" or "go" or "search")
+
+// **When the user enters a search term and presses enter (or clicks the button)**
+
+// * The form / title / description should hide
+// * Show a loading message (optional)
+// * Fetch related posts from reddit (with ajax)
+// * Display animation / slideshow of images (jQuery)
+// * Show a button to stop / reset the animation
+// * Repeat animation until use clicks "stop"
+
+// **When the user clicks the "stop" button**
+
+// * Animation stops / images are removed
+// * Form / title / description are shown again
+// * User can enter a new search term
+
+
+// ##Suggested proccess
+
+// It is important to break down any development project in to smaller pieces and tackle them one at a time. Here is a list of how you might want to attack this project.
+
+// * Create your form (html / css)
+// * Prevent default and show an alert on form submit
+// * Use AJAX to make a request. Show data in console
+// * Create an array of image URLs (tip: use [filter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) and [map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)).
+// * Make the form / title / description hide
+// * Cycle through images
+//     * tip: use [setInterval](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval)
+//     * Maybe just set the src attribute of a single image tag for now
+// * Add some interesting style / animation
+// * Create button to stop animation (tip: use [clearInterval](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/clearInterval)).
+var imageArray = [];
+var counter = 0;
+var timer;
+
+$('#myForm').submit(function (event) {
+  event.preventDefault();
+
+  var input = $('#query');
+  var userText = input.val();
+  $.get('https://www.reddit.com/search.json', {
+    q: userText,
+    nsfw: "no"
+  }).done(function (response) {
+    console.log(response);
+
+    var results = response.data.children
+    for (i = 0; i < results.length; i++) {
+      var redditData = results[i].data;
+      if (redditData.domain === "i.imgur.com" && redditData.url) {
+        imageArray.push(redditData.url);
+      }
+      console.log(imageArray);
+
+    }
+    // addSearchResult(imageArray);
+  triggerTimer();
+  });
+});
+
+function clearSearchResults() {
+  $("#results").html("");
+  $('#photo-div').html(""); 
+  $('#submit').fadeIn();
+  $('#submit').parent.fadeIn();
+  imageArray = [];
+}
+
+$('#submit')
+  .click(function () {
+    $(this).parent().fadeOut();
+    $(this).fadeOut();
+  });
+
+$('#stop-slides').click(function() {
+  console.log('stop click');
+  cancelTimer;
+  clearSearchResults();
+});
+
+
+
+function flashImage() {
+    if ($('#photo-div').html()) {
+      $('#photo-div').html("");
+    }
+    var html = '<img class="animate" src="' + imageArray[counter] + '"/>'; 
+    $('#photo-div').append(html); 
+    if (counter >= (imageArray.length-1)) {
+      cancelTimer();
+      imageArray = [];
+      clearSearchResults();
+    } else{
+      counter++;
+    }
+}
+
+
+
+function triggerTimer(){
+    timer = setInterval(flashImage, 4000);
+}
+
+function cancelTimer(){
+    clearInterval(timer);
+}
+
+
+
+  // CURRENTLY WORKING
+  // function flashImage() {
+  //   $("#slide").attr("src", imageArray[counter])
+  //   $(this).fadeIn();
+  //   counter++;
+  // }
+
+
+  // function addSearchResult(imageArray) {
+
+
+  //   setInterval(function () {
+  //     var html = '<img src="' + imageArray[i] + '"/>';
+  //     $('#photo-div').append(html);
+  //     i++;
+
+
+
+
+  //     if (i === imageArray.length) {
+  //       clearInterval(i);
+  //     }
+  //   }, 2000);
+
+
+  // }
+
+
+
+
+
+
+
+  //dfdfdfdfdfsdfsd
--- script.js
+++ script.js
@@ -1,62 +1,95 @@
-//stolen from Steve's code
+var imArr = [];
+var counter = 0;
+var nIntervId;
+
 
 $(function() {
   // attach the form submission to the search function
   $("#search-form").on('submit', search);
 });
 
-function search(event) {
-  // Stop the form from changing the page.
-  event.preventDefault();
-  clearSearchResults();
+  function search(event) {
+    // Stop the form from changing the page.
+    event.preventDefault();
 
-
-//data.children.preview
-
-  // Get the users search input and save it in a variable.
-  // Use the input placeholder value (like "kittens") as a default value.
-  var input = $("#query")
-  var userQuery =  input.val();
-  var imArr = [];
-
-  console.log("searching for:", userQuery);
-
-  $.get('https://www.reddit.com/search.json', {
-    q: userQuery
-  }).done(function(response) {
-    console.log(response);
+    var input = $("#query")
+    var userQuery =  input.val();
     
-  var results = response.data.children; 
-  for(var i=0; i < results.length; i++) {
-      var imageURL = results[i].data.url;
-      imArr.push(imageURL);
-      console.log(imArr);
-      addSearchResult(imageURL);
-    } 
+
+    $.get('https://www.reddit.com/search.json', {
+      q: userQuery
+      // nsfw: "no"
+    }).done(function(response) {
+      console.log(response);
+      
+    var results = response.data.children; 
+    for(i = 0; i < results.length; i++) {
+        var redditStuff = results[i].data;
+        if(redditStuff.domain === "i.imgur.com" && redditStuff.url) {
+        imArr.push(redditStuff.url);
+        console.log(imArr);
+        } 
+      }
+    changeSlide();
   });
+
+
+  // Clear previous search results.
+  function clearSearchResults() {
+    $("#results").html("");
+    $('#photo-div').html(""); 
+    $('#submit').fadeIn();
+    $('#submit').parent.fadeIn();
+    imaArr= [];
+  }
+
+  //hides button and input
+  $('#submit').click(function (){
+     $(this).parent().fadeOut();
+     $(this).fadeOut();
+    });
+
+
+  //stolen from Brent's code shamelessly
+  function flashImage() {
+      if ($('#photo-div').html()) {
+        $('#photo-div').html("");
+      }
+      var html = '<img class="animate" src="' + imArr[counter] + '"/>'; 
+      $('#photo-div').append(html); 
+      if (counter >= (imArr.length-1)) {
+        stopSlide();
+        imArr = [];
+        clearSearchResults();
+      } else{
+        counter++;
+      }
+  }
+
+
+  //timer stuff for slideshow
+  function changeSlide() {
+    nIntervId = setInterval(flashImage, 5000);
+  }
+
+  function stopSlide() {
+    clearInterval(nIntervId);
+  }
+
 }
 
-// Clear previous search results.
-function clearSearchResults() {
-  $("#results").html("");
-}
-
-// Adds a single result object to the page.
-function addSearchResult(imageArray) {
- for (i = 0; i < imageArray.length; i++) {
-   var eachImage = imageArray[i];
-   var html = '<img src="' + eachImage + '"/>';
-   $('#slideshow').append(html);
- }
-}
+// // Adds a single result object to the page.
+// function addSearchResult(arr) {
+//  for (i = 0; i < arr.length; i++) {
+//    var sepImage = arr[i];
+//    var html = '<img src="' + sepImage + '"/>';
+//    $('#slideshow').append(html);
+//  }
+// }
 
 
-//hides button and input
-$('input[name=pressMe]')
-.click(function (){
-   $(this).parent().fadeOut();
-   $(this).fadeOut();
-  });
+
+
 
 
 // $(window).load(function(){
