3a5e8d3c15218214edb0d97ac601bac3429c72cd
Paul <pacalabre@gmail.com>
2016-03-30 21:15:54 -0700

    tried various ways to add the photos to the slide show. I was able to add them to the screen, but not into a jQuery slider pluin, nor with a interval function and n array

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
+var photos = [];
 
-// Set up the page when it loads.
+
+
 $(function() {
-  // attach the form submission to the search function
   $("#search-form").on('submit', search);
 });
 
 function search(event) {
-  // Stop the form from changing the page.
   event.preventDefault();
-
   clearSearchResults();
-
-  // Get the users search input and save it in a variable.
-  // Use the input placeholder value (like "kittens") as a default value.
   var input = $("#query");
   var userQuery = input.val() || input.attr("placeholder");
-
   console.log("searching for:", userQuery);
-
   $.get('https://www.reddit.com/search.json', {
     q: userQuery
   }).done(function(response) {
@@ -25,11 +19,26 @@ function search(event) {
 
     var results = response.data.children;
     for (var i = 0; i < results.length; i++) {
-      var result = results[i].data.thumbnail;
-      $('#results').append('<img src="'+result+'">')
-       console.log(result);
+      var result = results[i].data.url;
+      photos.push(result);
+      console.log(photos);
     }
+    setInterval(function(){
+      var index = 0;
+      displayImage(photos[index]);
+      index = index++;
+    }, 3000);
   });
+}
+
+
+
+
+
+
+function displayImage(url){
+
+  $('#results').append('<img src="'+url+'">')
 }
 
 // Clear previous search results.
@@ -45,9 +54,8 @@ function addSearchResult(result) {
 
 
   // add the list item to the list of search results
-  $("#results").append(img);
+  $(".results").append(img);
 }
-
 
 
 
