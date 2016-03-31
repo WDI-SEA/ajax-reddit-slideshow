a89ba36bf60d1f63c58319f0564ed79a3e1b02f4
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-03-30 21:26:41 -0700

    Acheived minimum viable product

---
+++ script.js
@@ -0,0 +1,60 @@
+// Some dangerous global arrays
+var imageURLs = [];
+var imageIndex = 0;
+var currentImage;
+var intervalID;
+
+var stop = function(){
+	clearInterval(intervalID);
+	imageIndex = 0;
+	$('#div_image').empty();
+}
+
+var cycleImage = function(){
+	imageIndex++;
+	if (imageIndex < imageURLs.length){
+		currentImage[0].src = imageURLs[imageIndex];
+		currentImage[0].alt = imageURLs[imageIndex];
+	}
+	else {
+		stop();
+	}
+}
+
+// Add an event listener
+$("#form_search").submit(search);
+$("form_stop").submit(stop);
+
+function search(e) {
+	event.preventDefault();
+	stop();
+
+	// Grab the search parameter
+	if ($('#input_search').val() === ''){
+		return;
+	}
+	var searchStr = $('#input_search').val() + ' nsfw:no site:i.imgur.com';
+
+	// Do the search
+	console.log('Starting search for ' + searchStr);
+	$.get('https://www.reddit.com/search.json', {q: searchStr}).done(function(response){
+		console.log(response);
+
+		// Store the images urls
+		for (var i = 0; i < response.data.children.length; i++) {
+			var incomingString = response.data.children[i].data.url
+			imageURLs.push(incomingString.replace('gifv','gif'));
+		}
+
+		console.log(imageURLs);
+
+		currentImage = $('<img src="' + imageURLs[0] + ' alt="' + imageURLs[0] + '>"');
+
+		$('#div_image').prepend(currentImage);
+
+		imageIndex = 0;
+		intervalID = setInterval(cycleImage, 5000);
+	})
+}
+
+
