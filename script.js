fd9aed9c7943b4c3a1d74f0889cda1e57666a0b3
brytree <brycebortree@gmail.com>
2016-05-13 16:48:46 -0700

    fixed parts for Todd

--- script.js
+++ script.js
@@ -28,59 +28,60 @@ function search(event) {
 //changing based on Nat's code
 //this filters through the children to check for the preview tag & find the url
 //then pushes those urls into an array
-response.data.children.filter(function(result) {
-if(result.data.preview && result.data.preview.images[0].source.url) {
-    imArr.push(result.data.preview.images[0].source.url);
-    console.log(imArr);
-        } 
+  response.data.children.forEach(function(result) {
+    if(result.data.preview && result.data.preview.images[0].source.url) {
+        imArr.push(result.data.preview.images[0].source.url);
+        console.log(imArr);
+            } 
+          });
+        changeSlide();
       });
-    changeSlide();
-  });
 
 
-  // Clear previous search results.
-  function clearSearchResults() {
-    $("#results").html("");
-    $('#slide-show').html(""); 
-    imaArr= [];
-  }
-
-  //hides button and input
-  $('#submit').click(function (){
-     $(this).parent().fadeOut();
-     $(this).fadeOut();
-    });
-
-
-  //stolen from Brent's code shamelessly
-  //this checks the photo-div, clears it (from previous image), then animates the slide based off the image url
-  //if statement is the counter that allows the image to change through each of the search results
-  function flashImage() {
-      if ($('#slide-show').html()) {
-        $('#slide-show').html("");
+      // Clear previous search results.
+      function clearSearchResults() {
+        $("#results").html("");
+        $('#slide-show').html(""); 
+        imaArr= [];
       }
-      var html = '<img class="animate" src="' + imArr[counter] + '"/>'; 
-      $('#slide-show').append(html); 
-      if (counter >= (imArr.length-1)) {
-        stopSlide();
-        imArr = [];
-        clearSearchResults();
-      } else{
-        counter++;
-      }
-  }
 
-  //timer stuff for slideshow
-  function changeSlide() {
-    nIntervId = setInterval(flashImage, 3000);
-  }
+      //hides button and input
+      $('#submit').click(function (){
+         $(this).parent().fadeOut();
+         $(this).fadeOut();
+        });
 
-  function stopSlide() {
-    clearInterval(nIntervId);
-    $("#results").html("");
-    $('#slide-show').html(""); 
-    $('#submit').fadeIn();
-  }
+
+    //stolen from Brent's code shamelessly
+    //this checks the photo-div, clears it (from previous image), then animates the slide based off the image url
+    //if statement is the counter that allows the image to change through each of the search results
+    function flashImage() {
+        if ($('#slide-show').html()) {
+          $('#slide-show').html("");
+        }
+        var html = '<img class="animate" src="' + imArr[counter] + '"/>'; 
+        $('#slide-show').append(html); 
+        if (counter > (imArr.length)) {
+          stopSlide();
+          imArr = [];
+          clearSearchResults();
+        } else{
+          counter++;
+        }
+    }
+
+    //timer stuff for slideshow
+    function changeSlide() {
+      nIntervId = setInterval(flashImage, 3000);
+    }
+
+    function stopSlide() {
+      clearInterval(nIntervId);
+      $("#results").html("");
+      $('#slide-show').html(""); 
+      $('#submit').fadeIn();
+      counter === 0;
+    }
 
 }
 
