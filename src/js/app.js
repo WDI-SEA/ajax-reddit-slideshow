7eef75b2d86b6f269ab77b7e01c20ff3e875b24b
Ivar Lee <ivarr5@gmail.com>
2016-06-02 09:28:21 -0700

    js lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,32 @@
-$('document').ready(function(){
-
-
-
-
-
+$('document').ready(function() {
   var findPics = function(dataObj) {
     var filterResult = dataObj.data.children.filter(function(element) {
-      if (element.data.hasOwnProperty('preview')){
+      if (element.data.hasOwnProperty('preview')) {
         return true;
-      }else{
+      } else {
         return false;
       }
     });
 
-    console.log(filterResult);
-    var urlArray = filterResult.map(function(element){
+    var urlArray = filterResult.map(function(element) {
       return element.data.preview.images[0].source.url;
     });
 
-    console.log(urlArray);
+    // console.log(urlArray);
     var index = 0;
     intervalID = window.setInterval(function() {
-    index++;
-    if (index >= urlArray.length) {
+      index++;
+      if (index >= urlArray.length) {
         index = 0;
-    }
-    console.log(urlArray[index]);   // set new news item into the ticker
-    $('#results').html('<img src="' + urlArray[index] + '">');
-}, 5000);
+      }
+        // console.log(urlArray[index]);
+      $('#results').html('<img src="' + urlArray[index] + '">');
+    }, 5000);
   };
 
-
-
-
-  // var intervalElementSelector = setInterval(function(arr) {
-  //     index++;
-  //     if (index >= arr.length) {
-  //         index = 0;
-  //     }
-  //     console.log(arr[index]);
-  // }), 5000);
-
-  $("#textForSearch").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#buttonForSearch").click();
+  $('#textForSearch').keyup(function(event) {
+    if (event.keyCode === 13) {
+      $('#buttonForSearch').click();
     }
   });
 
@@ -53,40 +36,27 @@ $('document').ready(function(){
     var searchString = $('#textForSearch').val();
 
     $.get('https://www.reddit.com/search.json', {
-    q: searchString
-
-  }).done(function(data){
-    console.log('success!');
-    console.log(data);
-    myData = data;
-    findPics(myData);
-    // return myData;
-  }).fail(function(data){
-    console.log("failed!");
-    console.log(data);
-  }).always(function(data){
-    console.log('always');
-    console.log(data);
-    myData = data;
-  });
-
-    // var temp = $('#textForSearch').val();
-    // console.log(temp);
+      q: searchString
+    }).done(function(data) {
+      // console.log('success!');
+      // console.log(data);
+      findPics(data);
+    }).fail(function(data) {
+      // console.log('failed!');
+      // console.log(data);
+    }).always(function(data) {
+      // console.log('always');
+      // console.log(data);
+    });
     $('h1').hide();
     $('form').hide();
-
   });
-
-
 
   $('#buttonForStopSearch').on('click', function(e) {
     e.preventDefault();
-    // stop the animation with .clearInterval();
     window.clearInterval(intervalID);
     $('h1').show();
     $('form').show();
     $('#results').html('');
   });
-
-
-})
+});
