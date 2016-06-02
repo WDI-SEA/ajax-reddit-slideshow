c3ef72f79e22521f8cb4ea89f1a386decd02ecb4
Krista Calderon <kamsci@gmail.com>
2016-06-02 08:38:00 -0700

    ran lint manager and organized code with functions on top

--- src/js/app.js
+++ src/js/app.js
@@ -1,84 +1,69 @@
 $(document).ready(function() {
-
-// $.ajaxSetup({
-//  headers: {
-//    "Access-Control-Allow-Origin": true
-//  }
-// });
-$('#exit').hide();
-// Click function
-$('#startSearch').click(function() {
-  var query = $('#userInput').val().trim();
-  $.get('https://www.reddit.com/search.json', {
-    q: query,
-    // Fetch related posts from reddit with ajax
-    // q: $('#userInput').val()
-  }).done(function(data) {
-    $('#header').fadeOut(300);
-    $('#exit').show(20000);
-    var finalPicturesArray = getPicturesArray(data);
-    // console.log(finalPicturesArray);
-    startShow(finalPicturesArray);
-  }).fail(function(data) {
-  }).always(function(data) {
-  })
-});
-  //Display Images in an animation slideshow
-var myInterval;
-var intervalID = 0;
-// console.log(intervalID);
-
-function startShow(pictures) {
-  myInterval = setInterval(function() {
-    runShow(pictures);
-    // pictureFormat();
-    intervalID++;
-  }, 3000);
-  console.log("test");
-}
-$('#exit').click(function() {
-  clearInterval(myInterval);
-  myInterval = null;
-  $('#userInput').val("");
-  $('img').hide();
+// start without exit button
   $('#exit').hide();
-  $('#header').slideDown(300);
-
-});
-
-
-function runShow(pictures) {
-  // $('#pictureResults').html('<img src="' + pictures[intervalID] + '">');
-  $('#pictureResults').attr('src', pictures[intervalID]);
-  console.log(pictures[intervalID]);
-  console.log(intervalID);
-}
-
-function getPicturesArray(data) {
-  var childrenArray = data.data.children;
-  var showArray = [];
-  // loop through childrenArray to get images array
-  for (var i = 0; i < childrenArray.length; i++) {
-    // Filter out preview object to only those that have an image prop
-    var previewObject = childrenArray[i].data.preview;
-    var dataObject = childrenArray[i].data;
-    // console.log(previewObject);
-    if (dataObject.hasOwnProperty('preview')
-      && previewObject.hasOwnProperty('images')) {
-      var imageArray = childrenArray[i].data.preview.images;
-      for (var k = 0; k < imageArray.length; k++) {
-        var imageRecieved = imageArray[k].source.url;
-        showArray.push(imageRecieved);
+  // Variables
+  var myInterval;
+  var intervalID = 0;
+  // Functions
+  function runShow(pictures) {
+    // $('#pictureResults').html('<img src="' + pictures[intervalID] + '">');
+    $('#pictureResults').attr('src', pictures[intervalID]);
+    console.log(intervalID);
+  }
+  function startShow(pictures) {
+    myInterval = setInterval(function() {
+      runShow(pictures);
+      // pictureFormat();
+      intervalID++;
+    }, 3000);
+  }
+  function getPicturesArray(data) {
+    var childrenArray = data.data.children;
+    var showArray = [];
+    // loop through childrenArray to get images array
+    for (var i = 0; i < childrenArray.length; i++) {
+      // Filter out preview object to only those that have an image prop
+      var previewObject = childrenArray[i].data.preview;
+      var dataObject = childrenArray[i].data;
+      if (dataObject.hasOwnProperty('preview')
+        && previewObject.hasOwnProperty('images')) {
+        var imageArray = childrenArray[i].data.preview.images;
+        for (var k = 0; k < imageArray.length; k++) {
+          var imageRecieved = imageArray[k].source.url;
+          showArray.push(imageRecieved);
+        }
       }
     }
+    return showArray;
+  } // end of getPicturesArray
+  function stopShow() {
+    $('#exit').click(function() {
+      clearInterval(myInterval);
+      myInterval = null;
+      $('#userInput').val('');
+      $('img').hide();
+      $('#exit').hide();
+      $('#header').slideDown(300);
+    });
   }
-  return showArray;
-} // end of getPicturesArray
-
-// add pic formatting
-function pictureFormat() {
-  $('img').addClass('imageStyle');
-}
-// Stop an interval with clear interval
-
+  // Search and click to start api
+  $('#startSearch').click(function() {
+    var query = $('#userInput').val().trim();
+    $.get('https://www.reddit.com/search.json', {
+      q: query
+      // Fetch related posts from reddit with ajax
+    }).done(function(data) {
+      $('#header').fadeOut(300);
+      $('#exit').show(20000);
+      var finalPicturesArray = getPicturesArray(data);
+      startShow(finalPicturesArray);
+      stopShow();
+    }).fail(function() {
+      // Need to research when the api will fail
+        // $('.imageContainer').innerHTML('Sorry, no results.')
+        // $('#exit').hide();
+        // $('#header').slideDown(300);
+    }).always(function() {
+    });
+  });
 }); // end of doc ready
