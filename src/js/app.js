92e02b0c6f294a16f0459bfc1290dfe7c41da955
Ivar Lee <ivarr5@gmail.com>
2016-06-02 11:54:10 -0700

    added some comments to app.js to clear things up

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 $('document').ready(function() {
+
+  //findPics wittles down an object to an array of desired data
+  //by using .filter() to filter out objects that don't meet a certain requirement
+  //and by using .map()  to transform the filtered object in to an array of desired data
   var findPics = function(dataObj) {
     var filterResult = dataObj.data.children.filter(function(element) {
       if (element.data.hasOwnProperty('preview')) {
@@ -12,17 +16,19 @@ $('document').ready(function() {
       return element.data.preview.images[0].source.url;
     });
 
-    // console.log(urlArray);
     var index = 0;
     intervalID = window.setInterval(function() {
       index++;
       if (index >= urlArray.length) {
         index = 0;
       }
-        // console.log(urlArray[index]);
+
       $('#results').html('<img src="' + urlArray[index] + '">');
     }, 1000);
   };
+
+  //text inpout event listener that detects the enter key and
+  // registers a click event on the button
 
   $('#textForSearch').keyup(function(event) {
     if (event.keyCode === 13) {
@@ -30,6 +36,8 @@ $('document').ready(function() {
     }
   });
 
+  //button that pulls the value of the input in the text inpout
+  //and passes the text value as a variable to an AJAX call
   $('#buttonForSearch').on('click', function(e) {
     e.preventDefault();
 
@@ -38,20 +46,15 @@ $('document').ready(function() {
     $.get('https://www.reddit.com/search.json', {
       q: searchString
     }).done(function(data) {
-      // console.log('success!');
-      // console.log(JSON.stringify(data));
       findPics(data);
+    //a callback function that calls findPics upon a successful ajax call
     }).fail(function(data) {
-      // console.log('failed!');
-      // console.log(data);
     }).always(function(data) {
-      // console.log('always');
-      // console.log(data);
     });
     $('h1').hide();
     $('form').hide();
   });
-
+  //button that stops the interval function that began when findPics was called
   $('#buttonForStopSearch').on('click', function(e) {
     e.preventDefault();
     window.clearInterval(intervalID);
