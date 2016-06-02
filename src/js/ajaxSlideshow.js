e01361148f9bb8988a0437b5a3c1b47fa6c033ea
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-02 11:59:42 -0700

    it works

--- src/js/ajaxSlideshow.js
+++ src/js/ajaxSlideshow.js
@@ -1,59 +1,53 @@
 $(document).ready(function() {
-
   // $.ajaxSetup({
   //   headers: {
   //     "Access-Control-Allow-Origin": true
   //   }
   // });
-//create function to cycle through array of API data/img links and apply it to the #photo div
-function slideshow(pics){
-  pics.forEach(function (a){
-    console.log(a);
-    $('#photo').html('<img src="" />')
-    $('#photo').html('<img src="' + a + '" />');
-});
-}
+  //  create function to cycle through array of API data/img links and apply it to the #photo div
+  var count = 0;
+  var slideshow = function(pics) {
+    $('#show').attr('src', pics[count]);
+  };
 
-//clears the textfield when clicked on
+  //  clears the textfield when clicked on
   $('#input').click(function(event) {
     $('#input').val('');
   });
 
-//creat click event for when click on submit
-  $('#search').click(function(event){
+  //  creat click event for when click on submit
+  $('#search').click(function(event) {
     event.preventDefault();
-    //gets input value
+    //  gets input value
     var input = $('#input').val();
-    //puts value in query
+    //  puts value in query
     $.get('https://www.reddit.com/r/pics/search.json', {
-        q: input
-      }).always(function(data){
-        //hides the headers and submit button
-        $('#hide').addClass('hide')
-        var array = [];
-        //take data.data.children and if has property of preview, and images, loop through each images and push the url to the array
-        data.data.children.forEach(function(element){
-
-            if(element.data.hasOwnProperty('preview') && element.data.preview.hasOwnProperty('images')){
-
-              element.data.preview.images.forEach(function(e){
-
-                array.push(e.source.url);
-                var intervalID = window.setInterval(slideshow(array), 4000);
-              });
-            }
-
-          // console.log(array);
-          // return array;
-        });
+      q: input
+    }).always(function(data) {
+      //  hides the headers and submit button
+      $('#hide').addClass('hide');
+      var array = [];
+      //  take data.data.children and if has property of preview, and images, loop through each images and push the url to the array
+      data.data.children.forEach(function(element) {
+        if (element.data.hasOwnProperty('preview') && element.data.preview.hasOwnProperty('images')) {
+          element.data.preview.images.forEach(function(e) {
+            array.push(e.source.url);
+          });
+        }
       });
-
+      var intervalID = setInterval(function() {
+        slideshow(array);
+        if (count < array.length) {
+          count++;
+        } else {
+          count = 0;
+        } }, 4000);
+    });
     $('#input').val('');
   });
 
-  $('#reset').click(function(event){
+  $('#reset').click(function(event) {
     $('#hide').removeClass('hide');
     $('#photo').html('');
   });
-
 });
