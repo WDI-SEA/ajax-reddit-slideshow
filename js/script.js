ce478cb07b5b3621c6f166940063aaa348a53bc5
mullfuchs <burke.leo@gmail.com>
2016-07-29 01:16:40 -0700

    Adding final features and some minmal styling

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@ console.log("javascript");
 
 var currentSubreddit = "";
 var subredditData;
-var subredditImages = [];
 var imagesLoaded = false;
 
 
 $('#grabImageButton').click(function() {
   // body...
-  console.log("clicked");
-  currentSubreddit = $("#subreddit").val();
+  if(currentSubreddit == ""){
+     currentSubreddit = $("#subreddit").val(); 
+  }
+  else{
+    resetImageSlideshow();
+    currentSubreddit = $("#subreddit").val(); 
+  }
+
   getImages(currentSubreddit);
+  showHideForm();
 });
 
 function getImages(curSubReddit){
@@ -28,40 +34,87 @@ function makeSlideshow(images){
     } catch (error){
       console.log("image failed to load");
     }
-
-    //$('#slides').append("<img src=\"" + images[i] + "\">"); 
   }
 }
 
 function initializeSlideshow(){
   $("#slides").slidesjs({
     width: 940,
-    height: 528
+    height: 500,
+    play:{active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry. (ED: Thanks for nothing)
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 5000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: true,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+      },
+    navigation: {active: false, effect: "slide"}
   });
 }
 
 function loadImageArray(redditData){
   var postData = redditData.data.children;
+  var subredditImages = [];
   for(var i = 0; i < postData.length; i++){
-    if(!postData[i].data.over_18 && postData[i].data.preview.images.length == 1){
+    if(!postData[i].data.over_18 && checkForGifV(postData[i].data.url)){
       
       if(postData[i].data.domain === "i.imgur.com"){
-        //subredditImages.push(postData[i].data.url);
         subredditImages.push("<img src=\"" + postData[i].data.url + "\">");
-        //$("#slides").append("<img src=\"" + postData[i].data.url + "\">");
+        console.log(postData[i].data.url);
       }
 
       else if(postData[i].data.domain === "imgur.com"){
         TestURL = postData[i].data.url;
         if(!TestURL.includes("/a/") && !TestURL.includes("/gallery/")){
           subredditImages.push("<img src=\"" + postData[i].data.url + ".jpg\">");
+          console.log(postData[i].data.url);
         }
-        //$("#slides").append("<img src=\"" + postData[i].data.url + ".jpg\">");
       }
+
     }
   }
+  $('#container').append("<div id=\"slides\"></div>")
   makeSlideshow(subredditImages);
   initializeSlideshow();
+  $('.slidesjs-navigation').on('click', function(){
+    showHideForm();
+  });
+}
+
+function checkForGifV(item){
+  if(item[item.length - 1] === "v"){
+    console.log(item[item.length - 1]);
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+function showHideForm(){
+  if($("#formStuff").is(':visible')){
+    $("#formStuff").hide();  
+  }
+  else{
+    $("#formStuff").show();
+  }
+}
+
+$().click(function(){
+  console.log('clicked start stop');
+});
+
+function resetImageSlideshow(){
+  $('#container').empty();
 }
 
 
