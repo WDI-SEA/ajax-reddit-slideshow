92be01ce326a1b9d9e1607f370f206993ee074d6
shway250 <jackson_duhon@yahoo.com>
2016-08-28 23:08:21 -0700

    changed HTML CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
+$(document).ready(function() {
+
 var searchWord = $('#searchTerm');
 var button = $('#btn');
 
 
-button.on('click' ,function(){
-  if(!searchWord){
-    //jQuery function to inform user to enter text
-  }else{
-  console.log(searchWord.val());
-}
+// button.on('click' ,function(){
+//   if(!searchWord){
+//     //jQuery function to inform user to enter text
+//   }else{
+//   console.log(searchWord.val());
+//   search(searchWord.val());
+// }
+
+
+
+button.click(function() {
+
+  var inputText = searchWord.val();
+ $.ajax({
+      url: "https://www.reddit.com/search.json",
+      type: "get",
+      data: {
+      q: inputText
+   },
+  }).done(function(data) {
+    var posts = data.data.children;
+    //console.log(posts)
+    for(var i = 0; i<posts.length; i++){
+      if (posts[i].data.hasOwnProperty('preview')){
+        var pictureObj = posts[i].data.preview.images;
+        for(var j = 0; j < pictureObj.length; i++){
+          var imgURL = pictureObj[j].source.url;
+          $('#container').append("<img src=" + "'" + imgURL + "'" + ">");
+        }
+      }
+    }
+    
+      });
+   });
 });
