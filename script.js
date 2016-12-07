1e91687663a568fcb91ce278294280cbc4d5d805
Tobias Murphy <murphy.tobias@gmail.com>
2016-12-07 13:53:23 -0800

    Steve's notes added

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-$(document).ready(function() {
-    $('#searchform').submit(function(event) {
-        event.preventDefault();
-        $('#visible').hide();
+$(document).ready(handleDocumentReady);
 
-        var searchTerm = event.target.searchfield.value;
-        $.ajax({
-            url: 'https://www.reddit.com/search.json?q=' + searchTerm,
-            method: 'GET',
-            success: getImage,
-            error: function(response) {
-                console.log(response);
-            }
-        });
+function handleDocumentReady() {
+  console.log("ready function actually running");
+  $('#stop').show();
+  $('#searchform').submit(handleSubmit);
+  $("#stop").click(handleStop);
+}
+
+console.log("after ready function definition");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  $('#visible').hide();
 
 
-        function getImage(response) {
-            console.log(response);
-            var i = 0;
-            var posts = response.data.children;
-            // console.log(posts);
-            posts.forEach(function(post) {
-                if (post.data.preview) {
-                  setInterval(function() {
-                        if (i == slideshow.length) {
-                            i = 0};
-                        $('#slideshow').html("");
-                        $('#slideshow').append('<img src="' + post.data.preview.images[0].source.url + '">');
-                        i++;
-                    }, 2000);
-                    $('#slideshow').append('<img src="' + post.data.preview.images[i++].source.url + '">');
-                } else {
-                    console.log("no image");
-                }
-            });
-        }
-    });
-    $("#stop").click(function(){
-      clearInterval();
-      $("slideshow").html("");
-      $('#visible').show();
-    })
-});
+  var searchTerm = event.target.searchfield.value;
+  $.ajax({
+      url: 'https://www.reddit.com/search.json?q=' + searchTerm,
+      method: 'GET',
+      success: getImage,
+      error: function(response) {
+          console.log(response);
+      }
+  });
+}
+
+function handleStop() {
+  clearInterval();
+  $("slideshow").html("");
+  $('#visible').show();
+}
+
+// The response object looks something like this:
+// response {
+//   data: {
+//     children: [
+//       {data: { preview: "cat.png"}},
+//       {data: { preview: "dog.png"}},
+//       {data: { preview: "cow.png"}},
+//     ]
+//   }
+// }
+
+function getImage(response) {
+  console.log(response);
+  var i = 0;
+  var posts = response.data.children;
+  // console.log(posts);
+  setInterval(handleInterval, 2000);
+  posts.forEach(attachPostToSlideshow);
+}
+
+function attachPostToSlideshow(post) {
+  if (post.data.preview) {
+    var url = post.data.preview.images[0].source.url;
+    $('#slideshow').append('<img src="' + url + '">');
+  } else {
+    console.log("no image");
+  }
+}
+
+function handleInterval() {
+  if (i == slideshow.length) {
+      i = 0
+  };
+  $('#slideshow').html("");
+  $('#slideshow').append('<img src="' + post.data.preview.images[0].source.url + '">');
+  i++;
+}
