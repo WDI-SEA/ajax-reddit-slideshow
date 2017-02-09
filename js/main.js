c97fb3b68a89c44c0791f122538bfac2768a1ff7
kellyh6 <faithkeeping@yahoo.com>
2017-02-09 09:21:32 -0800

    Reddit Slide Show

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 
 $('#submit').click(function(){
 	var query = $('#userContent').val();
-	$.get('https://www.reddit.com/r/pics.json', {
-  		q: query
+	$.get('https://www.reddit.com/r/pics/search.json', {
+  		q: query,
+  		nsfw: 0,
+  		restrict_sr: "on",
+  		sort: "new"
   	}).done(function(data){
   		var contentPosts = data; //data obtained after click?
 		var contentObjects = contentPosts.data.children; //I don't know what this var is for
@@ -20,6 +23,5 @@ function postToPage(urls){
 	containerDiv.append('<a href=" '+urls.data.url +'">'+ urls.data.url+ '</a>') // I don't know what this line is doing to the page 
 	console.log(containerDiv); // is this the right var to console log?
 	
-	$('img').filter(query)
 }
 
