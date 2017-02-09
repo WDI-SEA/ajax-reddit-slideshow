9521a1701e3c8db590fe4a9d277f979631855591
m-woo <mark.chi.woo@gmail.com>
2017-02-09 00:59:29 -0800

    search bar pulling correct image links

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// console.log('1. Beginning of the page');
+
+// $('#button').click(function() {
+// 	var item = $('#toInput').val();
+// 	addNewItem(item);
+// });
+
+// function addNewItem(param){
+// 	$('#todoList').append('<li class="1">' + param + '</li>');
+// };
+
+$('#button').click(function() {
+	var item = $('#toInput').val();
+	$.get('http://www.reddit.com/r/pics/search.json', {    // /r/pics/
+		q: item,
+		restrict_sr: 'on',
+		sort: 'new',
+	}).done(function(data){     // a promise
+		var kitPosts = data;
+		console.log(kitPosts.data.children);
+		var kitObjects = kitPosts.data.children;    // sort through array 
+
+
+		for (var i = 0; i < kitObjects.length; i++) {
+			console.log(kitObjects[i]);
+			postToPage(kitObjects[i]);
+		}
+	}).fail(function(error){
+		console.log('error, sorry');
+	});
+});
+
+
+	function postToPage(urls){
+		var containerDiv = $('#links');
+		containerDiv.append('<a href = " ' + urls.data.url + ' " > ' +urls.data.url+ ' </a>')
+	};
+
