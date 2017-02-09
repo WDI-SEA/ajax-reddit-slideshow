741dd6d08bb760dd821a97ae567f32561bafee4c
kellyh6 <faithkeeping@yahoo.com>
2017-02-09 08:59:53 -0800

    Reddit Slide Show

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$('#submit').click(function(){
+	var query = $('#userContent').val();
+	$.get('https://www.reddit.com/r/pics.json', {
+  		q: query
+  	}).done(function(data){
+  		var contentPosts = data; //data obtained after click?
+		var contentObjects = contentPosts.data.children; //I don't know what this var is for
+		for(var i =0; i < contentObjects.length; i++){
+		postToPage(contentObjects[i]);
+  	}
+  	}).fail(function(error) {
+  console.log('An error occurred');
+});		
+})
+
+function postToPage(urls){
+	var query = $('#userContent').val();
+	var containerDiv = $('#links');
+	containerDiv.append('<a href=" '+urls.data.url +'">'+ urls.data.url+ '</a>') // I don't know what this line is doing to the page 
+	console.log(containerDiv); // is this the right var to console log?
+	
+	$('img').filter(query)
+}
+
