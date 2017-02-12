88ecd296c8bb6edd63bd41e95a9c2d0e83f6a2e9
m-woo <mark.chi.woo@gmail.com>
2017-02-11 16:10:25 -0800

    added slideshow

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
 
+var imageArray = [];
+
 $('#button').click(function() {
 	var item = $('#toInput').val();
 	$.get('http://www.reddit.com/r/pics/search.json', {    // /r/pics/
 		q: item,
 		restrict_sr: 'on',
 		sort: 'new',
-	}).done(function(data){     // a promise
-		var kitPosts = data;
-		console.log(kitPosts.data.children);
-		var kitObjects = kitPosts.data.children;    // sort through array 
-
-		for (var i = 0; i < kitObjects.length; i++) {
-			console.log(kitObjects[i]);
-			postToPage(kitObjects[i]);
-		}
+	}).done(function(data){
+		changeData(data.data.children);
 	}).fail(function(error){
 		console.log('error, sorry');
 	});
 });
 
-
-	function postToPage(urls){
-		var containerDiv = $('#links');
-		// if(url.contains('.jpg', '.png'){
-		containerDiv.append('<img src =   + urls.data.url +  >')
-
-		// })
+	function changeData(urls){
+		// console.log(urls);
+		for (var i = 0; i < urls.length; i++) {
+			imageArray.push(urls[i].data.url);
+		}
+		imageAttach();
 	};
+
+	function imageAttach(){
+		for (var i = 0; i < imageArray.length; i++){
+			$('#links').append('<img src="'+imageArray[i]+'">');
+		}
+		createSlideShow();
+	}
+
+function createSlideShow() {
+     $("#links").slidesjs({
+        width: 940,
+        height: 528,
+        pagination: {
+      	active: false,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+      });
+ };
+    
 
 
 
@@ -39,3 +55,5 @@ $('#button').click(function() {
 // set interval (set in the done function) : change the index of the array
 
 //loop, start the index at 1
+
+//stop button click, clear interval
