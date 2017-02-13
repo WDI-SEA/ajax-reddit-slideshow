9e25980db8c3aa5b342f97ec8e1cb05c71ddd0a0
cgnzlz22 <cgnzlz22@yahoo.com>
2017-02-12 17:09:25 -0800

    cgonzalez

--- js/app.js
+++ js/app.js
@@ -1,55 +1,64 @@
-$(document).ready(function() {
-  var myData;
-  var urls;
-    // hide stop button on page load.
-    $("#stopBtn").hide();
-
-    //After search button is clicked, hide h1,description and form. 
-    //Then show stop animation button.
-    $("#searchBtn").click(function() {
-        $("h1").hide();
-        $("#description").hide();
-        $("#searchForm").hide();
-        $("#stopBtn").show();
-    //Get posts from reddit
-        var search = $('#searchInput').val();
+$(document).ready(function(){
+//call the reddit api & get data from it based on the search query
+    $('#inputSearch').click(function(e){
+        var search = $('#input').val();
         $.get('https://www.reddit.com/r/pics/search.json', {
-            q: search
-        }).done(function(data) {
-            console.log(data);
-            myData = data;
-            logMyData();
-        }).fail(function(error) {
-            console.log('Something went wrong!');
-        }).always(function(data) {
-        });
+            q: search,
+            nsfw: 0,
+            restrict_sr: "on",
+            sort: "new"
+        }).done(function(data){
+            var imgPosts = data;
+            var imgObjects = imgPosts.data.children;
+            for (var i = 0; i < imgObjects.length; i++){
+                showResults(imgObjects[i]);
+            }
+            console.log();
+        }).fail(function(error){
+            console.log('There was an error :(');
+        })
+        e.preventDefault();
+        //After search button is clicked, hide h1,description and form. 
+        hideContainer();
     });
 
-    function logMyData() {
-        var filter = myData.data.children.filter(function(thisChild){
-          if (thisChild.data.thumbnail === "self" || thisChild.data.thumbnail === "") {
-            return false;
-            }else{
-            return true;
-          }
-        })
-        urls = filter.map(function(thisChild) {
-          return thisChild.data.thumbnail;
-        })
-        console.log(urls);
-        // createSlides();
+    function showResults(urls){
+        var containerDiv = $('#output');
+        var imgURL = urls.data.url;
+        if (imgURL){
+            containerDiv.append('<img class="scaled" src="'+imgURL+'">')
+        } else{
+            console.log('No images');
+        }
+        createSlides();
     }
 
-    // function createSlides(){
-    //     console.log("create slides");
-    //    }
+    function hideContainer(){
+        $('#container').hide();
+        $('#output').prepend("<button id='reset'>Reset</button>").append("<br>")
+        $('#reset').click(function(){
+            resetSearch();
+        })
+    }
+
+    function resetSearch(){
+        $('#container').show();
+        $('#input').val("").focus();
+        $('img').remove();
+        $('#reset').remove();
+    }
+    // createSlides
+    function createSlides(){
+        console.log("create slides");
+    }
 
 });
-// GAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!
 
 
-//get your textbox/submit button working
-//call the reddit api & get data from it based on the search query
+
+
+ 
+    
 //done function store the data in an array
 //set interval: change the index of the array
 //loop: start the index over at 0
