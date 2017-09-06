27c34a3bc6fd5df4cb0cf4a1cd4e69a2735f14a5
shway250 <jackson_duhon@yahoo.com>
2017-09-06 16:00:07 -0700

    added a comment to clarify part of my AJAX logic

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ button.click(function() {
     searchWord.focus();
     var posts = data.data.children;
       for(var i = 0; i<posts.length; i++){
+        //this is how I'm checking to make sure the reddit post is an image and not text
         if (posts[i].data.hasOwnProperty('preview')){
           var imgURL = posts[i].data.url;
           $('#container').append("<img src=" + "'" + imgURL + "'" + ">");
