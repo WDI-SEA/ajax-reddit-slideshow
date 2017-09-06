cda562704699889882eada1cf2cf22e1b8b5ce5b
shway250 <jackson_duhon@yahoo.com>
2017-09-06 15:58:26 -0700

    finished the ajax query, like a year later

--- js/script.js
+++ js/script.js
@@ -1,41 +1,22 @@
-$(document).ready(function() {
-
 var searchWord = $('#searchTerm');
 var button = $('#btn');
 
-
-// button.on('click' ,function(){
-//   if(!searchWord){
-//     //jQuery function to inform user to enter text
-//   }else{
-//   console.log(searchWord.val());
-//   search(searchWord.val());
-// }
-
-
-
 button.click(function() {
-
   var inputText = searchWord.val();
- $.ajax({
-      url: "https://www.reddit.com/search.json",
-      type: "get",
-      data: {
+  $.ajax({
+    url: "https://www.reddit.com/search.json",
+    type: "get",
+    data: {
       q: inputText
-   },
+    },
   }).done(function(data) {
+    searchWord.focus();
     var posts = data.data.children;
-    //console.log(posts)
-    for(var i = 0; i<posts.length; i++){
-      if (posts[i].data.hasOwnProperty('preview')){
-        var pictureObj = posts[i].data.preview.images;
-        for(var j = 0; j < pictureObj.length; i++){
-          var imgURL = pictureObj[j].source.url;
+      for(var i = 0; i<posts.length; i++){
+        if (posts[i].data.hasOwnProperty('preview')){
+          var imgURL = posts[i].data.url;
           $('#container').append("<img src=" + "'" + imgURL + "'" + ">");
         }
       }
-    }
-    
-      });
-   });
+  });
 });
