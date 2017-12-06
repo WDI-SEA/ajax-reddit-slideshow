16d37f3f6fa65a0023184284cd6141486e922c02
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2017-12-05 22:47:02 -0800

    functioning slide show - need to complete stop/reset buttons

--- js/script.js
+++ js/script.js
@@ -1,102 +1,90 @@
+//Set initial count = 0
+var count=0;
+
+var slideShowInterval;
+
+
 // Set up the page when it loads.
 $(function() {
-  // attach the form submission to the search function
   $('#search-form').on('submit', search);
 });
 
 
 function search(event) {
-
   // Stop the form from changing the page.
   event.preventDefault();
 
-  //clearSearchResults();
-
-  // Get the users search input and save it in a variable.
-  // Use the input placeholder value (like "kittens") as a default value.
-  var userInput = $('#query').val() || 'kittens';
-  //*NOTE: || assigns a default value of kittens if the first portion doesn't return anything
-  //*NOTE: empty string is a falsey value which is why it default to kittens
- // console.log('user input was', userInput);
-
+  var userInput = $('#query').val() || 'puppies';
   $.get('https://www.reddit.com/search.json', {
- // To change the # of results returned
     }).done(function(response) {
-   console.log(response.data.children); // Object of search results returned from Reddit
-    addSearchResult(response.data.children); // Sends data to addSearchResult Function
-  });
+      //console.log(response.data.children); 
+  
+      addslideShowElements();
+      slideControlCommands(); 
+      slideShowInterval =  setInterval(function(){interval(addSearchResult(response.data.children))},1000);
+    });
 
-  $("#searchInput").hide();
+    $("#searchInput").hide();
 }
 
-//Clear previous search results.
-// function clearSearchResults() {
-//   $('#w3-content w3-display-container').empty();
-// }
 
-
-// Adds a single result object to the page.
+// Return search results
 function addSearchResult(results) {
 
   var images = [];
-  
   for(var i = 0; i < results.length; i++) {
-  // Create a list item to contain the search result link
-      
-
-      // var source = img.src;
       var source = results[i].data.thumbnail;
+      // Validate image source URLs
       if (source === "self" || source === "default" || source === "image") {
         i++;
       } else {
-              images.push(source);
+        images.push(source);
       };
   };
 
-  var div = document.createElement('div');
-  var img = document.createElement('img');
-  $('#slideshow').append(div);
-  $(div).append(img);
-
-  function myStopFunction() {
-    clearInterval(interval);
-  }
-
-  var button = document.createElement('button');
-  $(button).attr("id", 'stopSlideShow').text("stop please");
-  $('#stopButton').append(button).click(myStopFunction);
-
-  var button = document.createElement('button');
-  $(button).attr("id", 'stopSlideShow').text("stop please");
-  $('#stopButton').append(button).click(myStopFunction);
-
-
-  var count = 0;
-  var interval = setInterval(function() {
-    console.log(images[count]);
-
-    $('img').attr("src", images[count]);
-    count++;
-    
-  },1000);
-
-
-  // $('#stopSlideShow').click(function () {
-  //   if (this.id == 'clicked') {
-        
-  //   };
-  // });
-
-  // var stopButton = document.createElement('button');
-  // $('#stopButton').attr("id", 'stopSlideShow');
-  // $('#stopSlideShow').append(stopButton);
-
+   return images;
 
 }
 
-// $('#stopSlideShow').click(function () {
-//     if (this.id == 'stopSlideShow') {
-//         count = 0;
-//     };
-//   });
+// Add DOM content for slide show images
+function addslideShowElements() {
+    var div = document.createElement('div');
+    var img = document.createElement('img');
+    $('#slideshow').append(div);
+    $(div).append(img);
+}
 
+// To display slide show control commands
+function slideControlCommands() {
+  document.getElementById('slideControl').style.display = 'block';
+
+}
+
+// To stop slide show
+function stopSlideShow() {
+    clearInterval(slideShowInterval);
+}
+
+
+// To reset interval
+function resetSlideShow() {
+    console.log('count'+count);
+  count = 0;
+    //console.log('count'+count);
+
+  slideShowInterval =  setInterval(function(){interval(addSearchResult(response.data.children))},1000);
+
+}
+
+// Create img source tags for slide show
+function interval(array) {
+    $('img').attr("src", array[count]).attr("id", "thumbnailImages");
+    count++;
+}
+
+
+// Load Page Content
+document.addEventListener("DOMContentLoaded", function() {
+  //document.getElementById('reset').addEventListener('click', reset);
+  document.getElementById('slideControl').style.display = 'none';
+});
