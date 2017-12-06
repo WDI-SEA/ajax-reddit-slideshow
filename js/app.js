a970654069154e8ea557f101a888def3021b9a67
adamredwoods <adamredwoods@users.noreply.github.com>
2017-12-06 02:10:34 -0800

    animation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 
-const RADIUS = 200;
+const RADIUS = 400;
 // const COUNTER_LIMIT = 10;
+var spot = [];
 var wheelTotal =0;
+var offset =0;
 
 //$(function() {
 
@@ -12,15 +14,37 @@ var wheelTotal =0;
 
    function incWheelCounter() {
       wheelCounter++;
+
       if (wheelCounter > wheelTotal) {
          wheelCounter =0;
+
       }
       return "#wheel"+wheelCounter+" img";
    }
 
    function animateWheel() {
       for (var i=0; i<wheelTotal; i++) {
+         let dest = "#wheel"+i;
+         // console.log(dest);
+         //$(dest).offset({top: spot[i].y, left: spot[i].x });
+         var j=i+offset;
+         if (j>wheelTotal-1) {
+            j=i+offset-wheelTotal;
+         }
 
+         $(dest).animate({
+            top:spot[j].y+"px",
+            left:spot[j].x+"px"
+         },1100,"linear");
+         $(dest+" img").animate({
+            width: 200*spot[j].size,
+            height: 200*spot[j].size
+         },1100);
+      }
+
+      offset++;
+      if (offset > wheelTotal) {
+         offset=0;
       }
    }
 
@@ -40,6 +64,10 @@ var wheelTotal =0;
       if(imgList.length>0) {
          setImageWheel(imgList[counter]);
          counter++;
+         if (counter>imgList.length) {
+            counter=0;
+            imgList = [];
+         }
       }
       //wheelCounter++;
 
@@ -47,22 +75,39 @@ var wheelTotal =0;
       animateWheel();
    }
 
+   // function getQuery(word) {
+   //    $.getJSON("https://www.reddit.com/r/pics.json", {
+   //      q: word,
+   //      limit:10
+   //    }).done( function(data) {
+   //       console.log(data);
+   //
+   //       imgList = [];
+   //
+   //       for(var i=0; i<data.data.children.length; i++) {
+   //          if (data.data.children[i].data.preview && data.data.children[i].data.preview.images) {
+   //             imgList.push(data.data.children[i].data.preview.images[0].source.url);
+   //          }
+   //       }
+   //       console.log("imglist ",imgList);
+   //       turnWheel();
+   //
+   //       //addSearchResult(data.data.children);
+   //    });
+   // }
+
    function getQuery(word) {
-      $.get("https://www.reddit.com/search.json", {
-        q: word,
-        nsfw: "no",
-        limit:10
-      }).done( function(data) {
+      $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+word+"&lang=en-us&format=json&jsoncallback=?").done( function(data) {
          console.log(data);
 
          imgList = [];
 
-         for(var i=0; i<data.data.children.length; i++) {
-            if (data.data.children[i].data.preview && data.data.children[i].data.preview.images) {
-               imgList.push(data.data.children[i].data.preview.images[0].source.url);
+         for(var i=0; i<data.items.length; i++) {
+            if (data.items[i] && i<wheelTotal) {
+               imgList.push(data.items[i].media.m);
             }
          }
-
+         console.log("imglist ",imgList);
          turnWheel();
 
          //addSearchResult(data.data.children);
@@ -76,9 +121,33 @@ var wheelTotal =0;
          $("#redditForm label input").val("");
       });
 
-      setInterval(turnWheel, 500);
+      setInterval(turnWheel, 1000);
 
       wheelTotal = $(".wheel-img").length;
+
+      let x=0, y=0;
+      let x0=$("#redditForm").offset().left+100, y0= $("#redditForm").offset().top-200;
+      let thetaInc = 2*Math.PI/wheelTotal;
+      let theta =0;
+      let sz=1.0;
+
+      for (var i=0; i<wheelTotal; i++) {
+         x = x0 + RADIUS*Math.cos(theta);
+         y = y0 - RADIUS*Math.sin(theta);
+         if (y0<y) {
+            sz=y0/y;
+         }
+         console.log(sz);
+         spot.push({
+            x: x,
+            y: y,
+            size: sz
+         });
+         theta = theta+thetaInc;
+
+         $("#wheel"+i).offset({top: spot[i].y+"px", left: spot[i].x+"px" });
+      }
+      console.log(spot);
    }
 
 
