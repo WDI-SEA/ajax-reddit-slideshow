a2de709ab088818c66d480fd1112dbe062e86fc5
cgroark <colingroark@gmail.com>
2017-12-05 22:14:03 -0800

    styled slides

--- js/script.js
+++ js/script.js
@@ -1,57 +1,39 @@
 var images = [];
 var count = 0;
+var reset = $("#reset");
+
 $(function() {
   $("#search-form").on("submit", search);
- 
-});
 
+});
 function search(event) {
   event.preventDefault();
   clearSearchResults();
-  
-
-var userInput = $("#query").val() || "search it here!";
-    console.log("user input was", userInput);
-    $("h1").css("display", "none");
-    $("#directions").css("display", "none");
-    $("form").css("display", "none");
-
+  var userInput = $("#query").val() || "search it here!";
+  $("h1").css("display", "none");
+  $("#directions").css("display", "none");
+  $("form").css("display", "none");
   $.get('https://www.reddit.com/search.json',{
   q: userInput,
-  limit: 15
+  limit: 50
 }).done(function(response) {
-  console.log(response.data.children);
   addSearchResult(response.data.children);
-});
-}
+  setInterval(function(){
+    count++
+    console.log(images[count])
+    $("#results").html(images[count])
+    }, 1500);
+  });
 
+}
 function clearSearchResults() {
   $("#results").html("");  
 }
-
 function addSearchResult(results) {
   for(var i=0; i<results.length; i++){
-   // console.log(results[i].data.title); 
    var image = document.createElement("img");
    image.src = results[i].data.thumbnail;
-
-   // $("#slideshow").append(image);
    images.push(image);
   }
-
-  
-  ///create an image and append it to DOM and give it an id
 }
-
-// var i=0;
-
-setInterval(function(){
-  console.log("check slides");
-    count++
-    $("#results").append(images[count]);
-    $("#results").append(images[count]);
-
-    // $("#results").remove(images[i++ -1]);
-}, 1000);
-// var slides = $("img").src
 
