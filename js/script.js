7d969039250b66e69cafcf71ef58f7cd99f2e4e3
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2017-12-05 22:17:15 -0800

    Updated styling and filtered out so only viable images get pulled into slideshow

--- js/script.js
+++ js/script.js
@@ -1,58 +1,66 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('script.js is loaded!');
-  $("#query").focus();
+document.addEventListener('DOMContentLoaded', function() {
+  $('#query').focus();
 
-var interval;
-var searchResults = {};
-var slideCount = 0;
+  var interval;
+  var searchResults = {};
+  var slideCount = 0;
+  var slideShowImages = [];
 
-$(function() {
-  $('#search-form').on('submit', search);
-});
+  $(function() {
+    $('#search-form').on('submit', search);
+  });
 
-$('#quit').click('click', 
-    function() {
-        clearInterval(interval);
-        $('#loading').text(''); 
-        $('#slideshow').css('background','transparent');
-        $('#search-bar').removeClass('inprogress'); 
-        $('#quit').removeClass('inprogress');
-        $('#slideshow').removeClass('inprogress');
-        $("#query").focus();
-});
+  $('#quit').click('click',
+        function() {
+          clearInterval(interval);
+          $('#loading').text('');
+          $('#slideshow').css('background', 'transparent');
+          $('#search-bar').removeClass('inprogress');
+          $('#quit').removeClass('inprogress');
+          $('#slideshow').removeClass('inprogress');
+          $('#query').focus();
+        });
 
-function search(event) {
-    event.preventDefault();  
-    $("#loading").text('Loading'); 
-    $("#search-bar").addClass('inprogress'); 
-    $("#quit").addClass('inprogress');
-    $("#slideshow").addClass('inprogress');
+  function addImages() {
+    for (var i = 0; i < searchResults.length; i++) {
+      var source = searchResults[i].data.url;
 
-    var userInput = $('#query').val() || 'kittens'; 
-    console.log(userInput);
+            // To filter out non-image extensions
+      var extension = source.split('.').pop();
+      if (extension === 'jpg' || extension === 'png' || extension === 'gif') {
+        slideShowImages.push(source);  // Adding viable image urls into this array
+      }
+    }
+  }
+
+  function search(event) {
+    event.preventDefault();
+    $('#loading').text('Loading');
+    $('#search-bar').addClass('inprogress');
+    $('#quit').addClass('inprogress');
+    $('#slideshow').addClass('inprogress');
+
+    var userInput = $('#query').val() || 'doggo';
 
     $.get('https://www.reddit.com/search.json', {
-        q: userInput + 'nsfw:no',
-        limit: 50       
+      q: userInput + 'nsfw:no',
+      limit: 300
     }).done(function(response) {
-        console.log(response);   
-        searchResults = response.data.children;
-        addSearchResult(); 
-        startSlideShow();
+      searchResults = response.data.children;
+      addImages();
+      addSearchResult();
+      startSlideShow();
     });
-}
+  }
 
-function startSlideShow() {
-    interval = setInterval(addSearchResult, 3000);
-}
+  function startSlideShow() {
+    interval = setInterval(addSearchResult, 4000);
+  }
 
-function addSearchResult() {  
-    $("#loading").text('Loading'); 
-    var source = searchResults[slideCount].data.url;
-    console.log(source);
-    $('#slideshow').css('background-image', 'url('+ source +')');
+  function addSearchResult() {
+    $('#loading').text('Loading');
+    var imageSource = slideShowImages[slideCount];
+    $('#slideshow').css('background-image', 'url(' + imageSource + ')');
     slideCount++;
-}
-
-
+  }
 });
