eb19ad14a54a04e1819f1cdaaf3227d0c10ad1d3
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2017-12-06 09:02:16 -0800

    flushed out more JS, not finished, will continue to work on it

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@ $(function() {
 });
 function search(event) {
   event.preventDefault();
+  console.log("search", $("#text").val());
 
+  var userInput = $("#text").val();
+
+	$.get('https://www.reddit.com/search.json', {
+	    q: userInput, 
+	  }).done(function(response) {
+	    console.log(response.data.children);
+	});
 };
