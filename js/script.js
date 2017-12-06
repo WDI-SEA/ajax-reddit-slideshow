dcaacce99efb726d480fb404f46445489e810561
ashco <ashchristie@gmail.com>
2017-12-05 18:29:29 -0800

    images displayed

--- js/script.js
+++ js/script.js
@@ -1,31 +1,62 @@
+//indicates if images are displayed
+var viewerStatus = false;
+
 var inputField = $('#inputField');
 var button = $('#button');
+var removeButton = 
+var displayedImgs = $('#displayedImgs');
 var userInput
 
-//run function on click
+//display imgs on click
 $(function() {
-  $(button).on('click', search);
+	$(button).on('click', search);
 });
 
+//remove img display on click
+$(function(){
+	$()
+})
+
+//convert input to variable
 function getInput(){
-	userInput = $('#inputField').val(); }
-
-//run 
-function search(event){
-	console.log('dev hell');
-	event.preventDefault();
-	clearSearchResults();
-
-	//assign user input to var
-	//call 
+	userInput = $('#inputField').val();
 }
 
-
-
-function lookUp(){
-
-}
-
+//clears returned images
 function clearSearchResults(){
-	$('#input').html('');
+	$(displayedImgs).html('');
 }
+
+//looks up userInput var, finds reddit pics
+function picLookUp(){
+	$.get('https://www.reddit.com/search.json', {
+		q: userInput + '+nsfw:no',
+		limit: 10
+	}).done(function(response) {
+		// console.log(response.data.children)
+  		createDisplay(response.data.children);
+	});
+}
+
+function createDisplay(results){
+	for(var i = 0; i < results.length; i++){
+		//declare variables
+		var img = document.createElement('img');
+		//create img gallary
+		img.src = results[i].data.thumbnail;
+		img.a = results[i].data.url;
+		$(displayedImgs).append(img);
+	}
+}
+
+//run on click function
+function search(event){
+	getInput();
+	event.preventDefault();
+	viewerStatus = true; 
+	clearSearchResults();
+	console.log('user input: ', userInput);
+	picLookUp();
+}
+
+
