6a060e8d36105ab2fe613410d32bbb668c466b8c
peterpickles <lipeter2006@gmail.com>
2018-01-01 10:44:01 -0800

    adding search logic and remove default event handling

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,6 +5,11 @@ function search(event) {
     event.preventDefault();
     var searchQuery = $("#query").val() || "babies";
 
-    $.get("https://www.reddit.com/search.json")
+    $.get("https://www.reddit.com/search.json", {
+    	q: searchQuery + "nsfw:no",
+    	limit: 100
+    }).done(function(data){
+    	console.log(data);
+    })
 
 }
