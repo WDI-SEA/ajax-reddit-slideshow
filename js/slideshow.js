09837d2bdae8be1c5c47796556c0db49fa055d66
peterpickles <lipeter2006@gmail.com>
2018-01-02 00:16:38 -0800

    finished slideshow logic

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,24 +1,35 @@
 //images stored in an array
-var imgs =[]; 
+var imgs =[];
+var searchResults = {};
 
 $(function() {
 	//attach form submit to a search function
 	$("#search-form").on("submit", search);
 });
 
+$("#stop").click("click", function(){
+	clearSearchResults();
+	$("#search=
+	searchResults = {};
+	imgs =[];
+
+});
+
 function search(event) {
 	//stop the default reload behavior of the form
 	event.preventDefault();
-	clearSearchResults();
-	$("search-form").css("display","none")
+	$("#loading").text("Loading");
+	$("#search-bar").addClass("searching");
+	$("#stop").addClass("searching");
+	$("#slideshow").addClass("searching");
 	//grab userInput, save it as userInput
 	//use babies as a search value
 	var userInput = $("#query").val() || "babies";
 	console.log("User Input was", userInput);
 
 	$.get("http://www.reddit.com/search.json", {
-	q: userInput,
-	limit: 10
+		q: userInput + "nsfw:no",
+		limit: 10
 	}).done(function(response){
 		console.log(response.data.children);
 		// addSearchResult(response.data.children);
