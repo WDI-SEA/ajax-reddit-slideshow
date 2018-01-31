72e6ed4e6fc35872dda25242d2e98f48a155ad61
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-01-31 01:02:15 -0800

    Tim Hass - jQuery Reddit Slideshow

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 $(document).ready( function(){
 
+  var progressInterval = null;
   var myInterval = null;
   var imageArray = [];
   var imgCounter = 0;
 
   var updateImage = function(){
     // hide the search field
-    console.log("update image " + imgCounter + " " + imageArray.length)
-
     if (imgCounter < imageArray.length){
       imgCounter++;
-        // add the item to the page
-      console.log("<img src='" + imageArray[imgCounter].data.thumbnail + "'>'");
-      $(".content img").remove();
-      $(".content").append("<img src='" + imageArray[imgCounter].data.thumbnail + "'>'");
-      // $(".content").append("<img src='" + imageArray + "'>'");
+        // replace content with html for current item on the page
+        $(".content").html("<img src='" + imageArray[imgCounter].data.thumbnail + "'>'");
     } else {
       imgCounter = 0;
       stopSlideInterval();
+      $( ".slideControls" ).accordion( "option", "active", 0 );
+      $(".instructions").show();
+      $("#searchTerm").val("");
+      $("#searchTerm").focus();
     }
-    // stopSlideInterval()
-    console.log(imgCounter);
   };
 
   function stopSlideInterval() {
@@ -28,8 +26,9 @@ $(document).ready( function(){
     }
 
   $("#searchButton").on("click", function(){
+    $( ".progressbar" ).progressbar({ value: false });
+    $(".instructions").hide();
     // if you use a submit button
-    // e.preventDefault();
     // get the value of the term to search for
     var searchString = document.forms["imageForm"].elements["searchTerm"].value;
     // use the requested value to find
@@ -37,47 +36,32 @@ $(document).ready( function(){
     $.get("https://www.reddit.com/search.json", {
       q: searchString
     }).done(function(data){
-      console.log(data);
-      // const result = words.filter(word => word.length > 6);
+      // store results from query
       var results = data.data.children;
-      // for each results look at the item
-      // filer the item where thumbnail === default
-      // imageArray = results.filter(results[data.thumbnail] !== "default");
+      // for each results look at the item filer the item where thumbnail === default
+      // imageArray = results.filter(data.data.children[result].data.thumbnail !== "default");
       for (result in results){
-        console.log("item " + result);
-          if (data.data.children[result].data.thumbnail !== "default"){
+          if (results[result].data.thumbnail !== "default"){
             imageArray.push(results[result]);
-            // console.log("results " + data.data.children[result].data.thumbnail)
           }
       }
-      $( "#slideControls" ).accordion( "option", "active", 1 );
-      myInterval = setInterval(updateImage, 3000);
+      $( ".slideControls" ).accordion( "option", "active", 1 );
+      $( ".progressbar" ).progressbar({ value: 75 }).delay( 2000 ).fadeOut( 400 );
+      myInterval = setInterval(updateImage, 1000);
+
     });
-  }); // searchButton
+  });
 
   // accordion
-  $("#slideControls").accordion();
-
-  $("#stopShow").on("click", stopSlideInterval);
-
-  // $( function(){
-  //   $("#button").on("click", function(){
-  //     $( "#myDiv" ).toggleClass( "newClass", 1000 );
-  //   });
-  // });
-
-  // progressbar
-  $( "#progressbar" ).progressbar({
-      // var myInterval = setInterval(function, milliseconds);
-      // myInterval.clearInterval();
-      // while(<100){}
-      // figure this out
-      // var myInterval = setInterval(function moveProgressBar(){
-      //   for (var i = 0; i < 10; i++){
-      //     value++;
-      //   }
-      //   clearInterval(myInterval);
-      // }, 3000);
+  $(".slideControls").accordion({
+    animate: 600
     });
+
+  // start and stop functions
+  $("#stopShow").on("click", stopSlideInterval);
+  $("#startShow").on("click", function(){
+    myInterval = setInterval(updateImage, 3000);
+  });
+  $("#searchTerm").focus();
 
 });// end of documentReady
