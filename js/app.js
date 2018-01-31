ff5b9da0454c6c407fd9481d45aa7cc527315b29
Chelsea Zhu <qianwei5668@gmail.com>
2018-01-30 16:39:30 -0800

    loaded_images

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $("button").on("click", function() {
+    // e.preventDefault();
+    // console.log("in the click");
+    var searchString = document.forms["imageform"].elements["imagebox"].value;
+    $.get('https://www.reddit.com/search.json', {
+      q: searchString
+    }).done(function(data) {
+      // var results = data.data.children;
+      var results = data.data.children;
+      console.log(data);
+      results.forEach(function(item) {
+        if (item.data.thumbnail !== 'default') {
+          $("#content").append("<img src='" + item.data.thumbnail + "'>");
+        }
+      });
+    });
+  });
+});
