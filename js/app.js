c838badc141806e58229638d9f7d9da5da66f84a
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-30 17:32:59 -0800

    new logic fo js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+var results= [];
+//how many images we've replaces
+var imgCounter = 1;
+//this is for the seconds interval
+var myInterval = null;
+
+//set itnerval function tht replaces images
+//two if else statments - one for img counter, one to replace image
+var updateImage = function() {
+  if (imgCounter < results.length) {
+    imgCounter++;
+  } else {
+    imgCounter = 0;
+    //how we move through the array by iteration using the imgCounter
+    $("img").attr('src', results[imgCounter]);
+  }
+  if (results[0].data.thumbnail !=== 'default') {
+    $('img').attr('src', results[0].data.thumbnail);
+  } else {
+    $('img').attr('src', results[1].data.thumbnail);
+  }
+}
+
+//on the click function, we start doing this
 $(document).ready(function() {
 
   $("button").on("click", function() {
@@ -8,16 +32,30 @@ $(document).ready(function() {
 
     var searchString = document.forms["imageform"].elements["imagebox"].value;
 
+
     $.get('https://www.reddit.com/search.json', {
       q: searchString
     }).done(function(data) {
       var results = data.data.children;
-      console.log(data);
-      results.forEach(function(item) {
-        if (item.data.thumbnail !== 'default') {
-          $("#content").append("<img src='" + item.data.thumbnail + "'>");
-        }
+
+      var filteredResults = results.filter(function(item)) {
+        return.item.data !== 'default';
       });
-    });
+
+      // if (results[0].data.thumbnail !=== 'default') {
+      //   $('img').attr('src', results[0].data.thumbnail);
+      // } else {
+      //   $('img').attr('src', results[1].data.thumbnail);
+      // }
+      myInterval = setInterval(updateImage, 3000);
+
+      });
+
+
+    }
+  });
+});
+
+
   });
 });
