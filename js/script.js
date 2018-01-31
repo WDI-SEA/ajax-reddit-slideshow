445e861d2975071a3621c54ff51e57e86c4d6f76
Zakir Butte <zakir.butte@gmailcom>
2018-01-31 07:51:13 -0800

    reddit API built but not working properly

---
+++ js/script.js
@@ -0,0 +1,66 @@
+var i = 1;
+
+var slideshow = function (pictureArray) {
+  $('#picture').attr('src', pictureArray[i]);
+  i = i + 1;
+};
+
+var hideOnClick = function() {
+
+};
+
+
+var crawl = function () {
+
+  var searchString = $('#searchbox').val();
+    console.log($('#searchbox').val());
+    $.get('https://www.reddit.com/search.json', {
+        q: searchString   // search string for results, when DONE, next function starts
+      }).done( function(data) {           //store results in an array from children
+          var results = data.data.children;
+          console.log(data);
+
+        var imageUrls = results.map(function(item) {
+            if (item.data.thumbnail.indexOf("reddit") != -1) {
+                var urlArray = [];
+                urlArray.push(item.data.thumbnail);
+              return urlArray;
+             }
+          }); 
+        console.log(imageUrls);
+
+        $('.container').addClass('hide');
+        $('#stopButton').removeClass('hide');
+
+         var imageTimer = function () {
+          setInterval(slideshow(imageUrls), 1000);
+        };
+         imageTimer();
+
+         var stopSlideshow = function () {
+          clearInterval(imageTimer)
+        };
+
+        $('#stopButton').on('click', stopSlideshow);
+
+
+     });
+
+};
+
+
+$(document).ready(function() {
+  
+  $("#searchButton").on("click", function () {
+    // e.preventDefault();
+    // console.log("in the click");
+    crawl();
+
+     //outside functions end below
+  });
+});
+
+
+// $("#pictures").append("<img src='" + item.data.thumbnail + "'>");
+
+// var slideshowTimer = setInterval(slideshow, 1000);
