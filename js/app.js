b7c38e3945b187a61185cdbc2db36a547d984c84
Scott Molsness <smolsness@gmail.com>
2018-02-01 08:40:19 -0800

    fixed reset function

--- js/app.js
+++ js/app.js
@@ -1,57 +1,54 @@
-var imgArray = [];
-var imgNumber = 0;
-var reset = $("#reset");
 
-var nextImage = function() {
-  if (imgNumber < imgArray.length) {
-    imgNumber++;
-  } else {
-    imgNumber = 1;
-  }
-  $("img").attr("src", imgArray[imgNumber]);
-};
-
-var imgInterval = setInterval(nextImage, 3000);
-
-var stopInterval = function() {
-  imgArray = [];
-  clearInterval(nextImage);
-  $("img").attr("");
-  $("splash").show();
-}
 
 $(document).ready(function() {
 
-  $(document).keypress(function(event) {
-    if(event.which == 13) {
-      search();
-      return false;
-    };
-  });
+  var imgArray = [];
+  var imgNumber = 0;
 
-  var search = function() {
-    $("#searchbutton").on("click", function(event) {
-      event.preventDefault();
-    });
-    // console.log("in the click");
+  var nextImage = function() {
+    if (imgNumber < imgArray.length) {
+      imgNumber++;
+    } else {
+      imgNumber = 1;
+    }
+    $("img").attr("src", imgArray[imgNumber]);
+  };
+
+  var imgInterval = setInterval(nextImage, 3000);
+
+
+    var searchFunction = function() {
+
     var searchString = document.forms["imageform"].elements["imagebox"].value;
 
     $.get('https://www.reddit.com/search.json', { //first parameter is url
       q: searchString             //second parameter is object
     }).done(function(data) {
       var results = data.data.children;
-      console.log(data);   //make sure this works and look through to find where data is stored using dropdown arrows
       results.forEach(function(item) {
-        if (item.data.thumbnail !== "default") {
-          imgArray.push(item.data.thumbnail);
+        if (item.data.url.indexOf('.jpg') !== -1 || item.data.url.indexOf('.png') !== -1 || item.data.url.indexOf('.gifv') === 1 || item.data.url.indexOf('.tiff') !== -1 ) {
+          imgArray.push(item.data.url);
         };
       });
-      nextImage();
       $("img").addClass("resultImg");
       $("img").attr("src", imgArray[0]);
       $("#splash").hide();
-      $("#content").append("<button type='button' id='reset'>"+"Reset"+"</button>");
-      reset.on("click", stopInterval);
+      $("#reset").css("display", "block");
+      nextImage();
     });
-  };
+  };   // End search function
+
+  $("#searchbutton").on("click", searchFunction)
+
+  $("#reset").on("click", function() {
+    imgArray = [];
+    clearInterval(imgInterval);
+    $("#content").hide();
+    $("#splash").show();
+    $("#reset").css("display", "none");
+    $("#searchterm").val('');
+  });  //End stop function
+
+  $("#searchterm").focus();
+
 });
