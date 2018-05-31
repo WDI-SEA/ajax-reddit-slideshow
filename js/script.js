051beb998db96089e3ab64ede74c0b01b5750486
Genevieve Schultz <genschultz@gmail.com>
2018-05-31 09:16:55 -0700

    updated js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 $(document).ready(function() {
   console.log("jQuery loaded");
   
-  // Query reddit for images and display on page
+  // Cursor is focused in search box
+  $("#search").focus();
+  // Query reddit for images
   $('form').on('submit', function(){
     var userInput = $("[type='text']").val();
     event.preventDefault();
-    // $(".search-box").attr("hidden", true);
-    // $(".slideshow").attr("hidden", false);
+    $(".search-box").css("display", "none");
+    $(".slideshow").css("display", "inline-block");
     $.get('http://www.reddit.com/r/aww/search.json', {
       q: userInput + '+nsfw:no'
     }).done(function(returnData) {
@@ -16,14 +18,19 @@ $(document).ready(function() {
       var hasThumbs = returnData.data.children.filter(redditEntry => redditEntry.data.thumbnail != null)
       hasThumbs.forEach(function (item, i) {
         console.log(item.data);
+        //display images on page
         $('.img-box').append("<a href='" + item.data.url + "' target='blank'><img src='" + item.data.thumbnail + "'/></a>");
       })
     });
   });
-  // Reset Page
+  // Reset Page for a new search
   $('#reset-button').on('click', function() {
-    // $(".slideshow").attr("hidden", true);
-    // $(".search-box").removeAttr("hidden");
+    $(".search-box").css("display", "inline-block");
+    $(".slideshow").css("display", "none");
+    $("input[type='text']").val("");
+    $("#search").focus();
+    // Remove data/images from previous search
+    
   })
 });
 
