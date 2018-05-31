4c66dfb61f4dcf962fb01153d8eef20b942071f6
shullmb <shullmb@gmail.com>
2018-05-31 08:28:26 -0700

    cleaned up code and added error handling

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 var searchInput;
 var intervalHandle;
 
-// function to show slides
-// var showSlide = function(i) {
-//     // i > 0 ? $('#stage').remove() : $('#stage'.remove(slide[0]))
-//     // $('#stage').append[slides[i]];
-    
-    
-// }
 // ajax call to reddit
 var getRedditResults = function(input) {
     $.get('http://www.reddit.com/search.json', {
@@ -16,46 +9,50 @@ var getRedditResults = function(input) {
         var slides = [];
         var urls = [];
         returnData.data.children.forEach(function(listing){
-            // test to see if image url exists
+            // test to see if image url exists, filter out gifv and collect
             var urlArr = listing.data.url.split('.')
-            var imgExt = new RegExp('jpg|jpeg|png')
+            var imgExt = new RegExp('gif(?!v)|jpg|jpeg|png')
+
             if (urlArr[urlArr.length - 1].match(imgExt)) {
                 urls.push(listing.data.url);
             }
         })
-        // console.log(urls);
+        // map to html elements
         slides = urls.map( function(url){
-            return "<div class='img' id='slide'><img src='" + url + "'></div>";
+            return "<div id='slide'><img src='" + url + "' width='800px'></div>";
         })
-
+        // cycle through slides arrayw
         intervalHandle = setInterval( function(){
             $('#slide').fadeIn(500);
             $('#slide').replaceWith(slides[0]);
             let slideShown = slides.shift();
             slides.push(slideShown);
-            $('#slide').fadeOut(2000);
+            $('#slide').fadeOut(2900);
         },3000)
+    }).fail( function() {
+        $('#slide').html("<div class='error'><h1>Uh oh! something went wrong! Try again!</h1></div>");
     })
 }
 
-
 $(document).ready(function() {
+    $('#query').focus();
     // click search button to initiate ajax call and slideshow
     $('#search').click( function(e) {
         e.preventDefault();
         searchInput = $('#query').val();
         $('#form-tab').slideUp('slow');
         getRedditResults(searchInput);
-        $('#slide').html('<h2>loading...');
+        $('#slide').html('<h2>loading...</h2>');
     });
 
+    // bind stop button to stop slide show and clear the slide div
     $('#stop').click( function() {
+        $('#query').val('');
         $('#form-tab').slideDown('slow');
         clearInterval(intervalHandle);
         $('#slide').empty();
+        $('#query').focus();
 
     })
-
-
 
 })
