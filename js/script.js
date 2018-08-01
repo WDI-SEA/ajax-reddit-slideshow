e6d3ca01a9e087fb9a01d50441a6f91172c69d44
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-07-31 23:01:24 -0700

    added arrays and search button; search and slideshow nonfunctional

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
-console.log("Hello World");
 
-$.ajax({
-	url: "https://www.reddit.com/search.json",
-	method: "GET",
-	data: {
-		q: "dinosaur costume"
-	}
 
-}).done(function(response){
-	console.log("response.data", response.data);
-	response.data.children.forEach(function(post){
-		if (post.data.url.includes(".jpg")) {
+$(document).ready(function() {
+	
+	console.log("Hello World");
 
-		console.log(post.data.title);
-		console.log(post.data.url);
-		var newDiv = $("<div></div>").html("<img src=\"" + post.data.url + "\">");
-		$("body").append(newDiv);	
-		};
-	})	
-}).fail(function(err){
-	console.log("error", err);
+	var slideshowPix = [];
+
+	
+	//function redditQuery() {
+		//var userQuery = document.getElementById("user-search").value;
+		$.ajax({
+			url: "https://www.reddit.com/search.json",
+			method: "GET",
+			data: {
+				q: "dinosaur costume"
+			}
+
+		}).done(function(response){
+			console.log("response.data", response.data);
+			response.data.children.forEach(function(post){
+				if (post.data.url.includes(".jpg")) {
+
+				console.log(post.data.title);
+				//console.log(post.data.url);
+				slideshowPix.push(post.data.url);
+
+				};
+				console.log(slideshowPix);
+			})	
+
+		}).fail(function(err){
+			console.log("error", err);
+		});
+	//};
+
+    function slideShow() {
+    	var current = 0;
+    	var imgDuration = 3000;
+        document.getElementById("img1").src = slideshowPix[current];
+        current++;
+        if (current == slideshowPix.length) { current = 0; }
+        setTimeout("slideShow()", imgDuration);
+    };
+
+	$("button").click(function(){
+			console.log("click happened");
+			//redditQuery();
+			slideShow();
+	});
+
+
+
 });
