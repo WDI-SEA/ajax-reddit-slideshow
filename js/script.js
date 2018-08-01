04ea2fcddc3a5b4e927946a9b2918a3a4abca610
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-07-31 22:45:48 -0700

    Search and button on HTML. Linking AJAX to API results in errors

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-console.log("hello world!");
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("dom content loaded");
+});
+// Create your form (HTML/CSS)
+// Prevent default form submission and verify that you can type something into the form
+// Use AJAX to make a request. Show data in console
+// Create an array of image URLs (tip: use filter and map).
+// Make the form / title / description hide
+// Cycle through images
+// tip: use setInterval
+// Either add images, or change the src of a single image tag
+// Add some interesting style / animation
+// Create button to stop animation (tip: use clearInterval).
 
-$("#imagebutton").on('click', function(){
+
+$("#searchbutton").on('click', function(){
 	console.log("button clicked");
 
 	$.ajax({
-		url: "https://talaikis.com/api/quotes/random/",
+		url: "https://www.reddit.com/search.json?",
 		method: "GET"
+		data: {q: $("#searchbox").val()}
 	}).done(function(response){
-		console.log("success", response);
-		$("#title").text(response.author + " on " + response.cat);
-		$("#message").text("\"" + response.quote + "\"");
-		$("#message").css("font-style", "italic"); 
+		console.log("success", response.data.children);
+		$("#image").url(response.data.children.data);
 	}).fail(function(err){
 		console.log("error", err);
 	})
