d431b9ceb2c024faa26c0f3183e9e300c4e2c9d1
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-05 12:10:00 -0700

    build ajax call

--- js/main.js
+++ js/main.js
@@ -6,22 +6,38 @@ $(document).ready(function() {
 // process user input
 //
 function runSearch(e) {
+  // prevent page refresh on submit (and other defaults)
   e.preventDefault();
-  console.log('submit sent, event is: ', e);
-  console.log('event target is: : ', e.target);
-  console.log('event target[0] is: : ', e.target[0]);
-  console.log('event target[0].value is: : ', e.target[0].value);
+  // output input field contents CAUTION: probably html struc dependent
+  console.log('submit sent, event is:', e);
+  console.log('event target is: :', e.target);
+  console.log('event target[0] is: :', e.target[0]);
+  console.log('event target[0].value is: :', e.target[0].value);
+  // set input field content
+  var search = e.target[0].value;
+  // run ajax to get search results
+  scanReddit(search);
 }
-
-//
-// user input
-//
-// get user input
-$('#search').submit(runSearch);
 
 //
 // ajax call
 //
+function scanReddit(query) {
+  console.log('search is: ', query + '+nsfw:no');
+  $.ajax({
+    url: 'https://www.reddit.com/search.json?q=',
+    method: 'GET',
+    data: query + '+nsfw:no'
+  }).done(function(data) {
+    console.log('success with result:', data);
+  }).fail(function(data) {
+    console.log('fail with feedback:', data);
+  });
+}
+
+// setup event listener on submit button
+$('#search').submit(runSearch);
+
 
 //
 // output
