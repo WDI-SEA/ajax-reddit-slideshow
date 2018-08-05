9e37697d7d0e39066596f0e6f84d0cc5a0964361
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-05 10:33:05 -0700

    console log input field

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
   console.log('DOM and JQuery loaded');
 });
+
+//
+// process user input
+//
+function runSearch(e) {
+  e.preventDefault();
+  console.log('submit sent, event is: ', e);
+  console.log('event target is: : ', e.target);
+  console.log('event target[0] is: : ', e.target[0]);
+  console.log('event target[0].value is: : ', e.target[0].value);
+}
+
+//
+// user input
+//
+// get user input
+$('#search').submit(runSearch);
+
+//
+// ajax call
+//
+
+//
+// output
+//
