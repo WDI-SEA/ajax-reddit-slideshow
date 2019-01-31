67e69cebcaf7b2e13251f56c9c9cf175c255ed96
Carlo Bruno <carlofbruno@gmail.com>
2019-01-31 11:15:39 -0800

    refactor fetch call moved elements around worked on css a bit TODO: work on css some more

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // selectors
+let queryBox = document.querySelector(".query-box");
 let input = document.querySelector("#query");
-let search = document.querySelector("#search");
+let searchBtn = document.querySelector("#search");
 
 let imageBox = document.querySelector(".image-box");
+let imageDiv = document.querySelector(".image");
+let stopBtn = document.querySelector("#stop");
 
 // Functions
 
-document.forms["query-form"].addEventListener("submit", function(
-  e
-) {
-  e.preventDefault();
+searchBtn.addEventListener("click", function(e) {
   let query = input.value;
   searchReddit(query);
 });
@@ -22,22 +22,45 @@ function searchReddit(query) {
     .then(data => {
       return data.json();
     })
-    .then(data => {
-      return data.data.children;
-    })
-    .then(children => {
-      // filter post with image links
-      return children.filter(child => {
+    .then(json => {
+      // json -> filter -> post with image links
+      let urlList = json.data.children.filter(child => {
         return (
           // child.data.url.match(/\.(jpeg|jpg|gif|png|gifv)$/) != null
           child.data.post_hint === "image"
         );
       });
-    })
-    .then(urlList => {
-      // posts with image links
+      // array of posts with image links -> array of image links
       let imageArr = urlList.map(entry => entry.data.url);
-      // console.log(imageArr);
-      // slideShow(imageArr);
+      console.log(imageArr);
+      slideShow(imageArr);
     });
 }
+
+var intHandler;
+
+function slideShow(arr) {
+  queryBox.classList.toggle("hidden");
+  imageBox.classList.toggle("hidden");
+  let i = 0;
+
+  function changeImage() {
+    console.log(i);
+    let newImage = document.createElement("img");
+    newImage.src = arr[i];
+
+    imageDiv.innerHTML = "";
+    imageDiv.appendChild(newImage);
+    if (i < arr.length - 1) i++;
+    else i = 0;
+  }
+  intHandler = setInterval(changeImage, 3000);
+}
+
+stopBtn.addEventListener("click", () => {
+  console.log("stop");
+  imageDiv.innerHTML = "";
+  queryBox.classList.toggle("hidden");
+  imageBox.classList.toggle("hidden");
+  clearInterval(intHandler);
+});
