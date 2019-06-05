28856e7b4ddea29cfec768540a757a88bc3e883b
Kelsey Cox <kelseycox@Lucy.local>
2019-06-05 11:18:02 -0700

    coding along

--- js/script.js
+++ js/script.js
@@ -1,46 +1,64 @@
-//variables
-var array = [];
-
 //html elements
-var inputEl = document.querySelector('[name="search"]');
-var btnEl = document.getElementById("submit");
-var stopEl = document.getElementById("stop");
-var pictureEL = document.getElementById("picture")
+// var inputEl = document.querySelector('[name="search"]');
+// var btnEl = document.getElementById("submit");
+// var stopEl = document.getElementById("stop");
+// var pictureEL = document.getElementById("picture");
+
+var submitBtn;
+var inputEl;
+var pictureEl;
+var splash;
+var searchTerm = '';
+var searchUrl1 = 'https://www.reddit.com/search.json?q='
+var searchUrl2 = '+nsfw:no';
+var url = '';
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    inputEl = document.querySelector('[name="search"]');
+    submitBtn = document.getElementById("submit");
+    pictureEL = document.getElementById("picture")
+    splash = document.getElementById("splash")
+    
+    submitBtn.addEventListener("click", function(e) {    
+        searchTerm = inputEl.value;
+        inputEl.value = '';
+        url = searchUrl1 + searchTerm + searchUrl2;
+        fetch(url)
+            .then(function(data) {
+                return data.json();
+            })
+            .then(function(json) {
+                var posts = json.data.children[1].data.thumbnail;
+                console.log(posts);
+                pictureEl.src = json.data.children[1].data.thumbnail
+        });
+    });
+});
+
+
+
 
 
 //functions
-function startSearch() {
-    fetch('https://www.reddit.com/search.json?q=' + inputEl.value + '+nsfw:no')
-    .then(function(data) {
-        return data.json();
-    })
-    .then(function(json) {
-        var posts = json.data.children;
-        console.log(posts);
-        for (i = 0; i < posts.length; i++) {
-            array.push(posts[i].data.thumbnail);
-        }
-    })
-}
+// function startSearch() {
+// }
 
-function cyclePics(array) {
-    setInterval(function(e) {
-        var i = 0;
-        pictureEL.src = array[i];
-        i++
-    }, 6000)
+// function cyclePics(ticker) {
+//     pictureEL.src = picArray;
+// }
 
-}
+// var interval = setInterval(cyclePics, 6000);
 
 //event listeners
-btnEl.addEventListener("click", function(t) {    
-    var textInput = inputEl.value;
-    startSearch(textInput);
-    inputEl.value = '';
-    cyclePics();
-});
+// btnEl.addEventListener("click", function(t) {    
+//     var textInput = inputEl.value;
+//     startSearch(textInput);
+//     inputEl.value = '';
+//     interval;
+// });
 
-stopEl.addEventListener("click", function(t) {
+// stopEl.addEventListener("click", function(t) {
     //do the clear interval thing.
     //make buttons come back
-});
+// });
