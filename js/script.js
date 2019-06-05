f400e30a9370818c4015fc566030fcf26ae2f55b
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-05 11:54:02 -0700

    updated program logic

--- js/script.js
+++ js/script.js
@@ -2,17 +2,73 @@
 var values = [];
 var imagesArray = [];
 var slideIndex = 0;
-var redditUrl = "https://www.reddit.com/search.json?q=";
+var redditUrlFirst = "https://www.reddit.com/search.json?q=";
+var redditUrlLast = "+nsfw:no";
+var url = "";
 var searchWordArray = [];
 var searchWord = "";
+var clearInterval;
+var wordInput = "";
+var imageIndex = 0;
+var handle = null;
 
-var description = document.getElementById("description");
-var playBttn = document.getElementById("play");
-var stopBttn = document.getElementById("stop");
-var textInput = document.getElementById("text");
-var slide = document.getElementById("slides");
+var description;
+var playBttn;
+var stopBttn;
+var textInput;
+var slide;
+
+document.addEventListener("DOMContentLoaded", function() {
+  description = document.getElementById("description");
+  playBttn = document.getElementById("play");
+  stopBttn = document.getElementById("stop");
+  textInput = document.getElementById("text");
+  slide = document.getElementById("slides");
+
+  playBttn.addEventListener("click", function(e) {
+    wordInput = textInput.value;
+    searchWord = String.fromCharCode(e.which).toLowerCase();
+    textInput.value = ""; 
+    url = redditUrlFirst + wordInput + redditUrlLast;
+
+    fetch(url)
+      .then(function(data) {
+        return data.json();
+      })
+      .then(function(json) {
+        //console.log(json.data.childred[1].data.thumbnail);
+        var newThumbs = json.data.children.map(function(thumb) {
+          return thumb.data.thumbnail;
+        });
+        slide.src = newThumbs[0];
+        handle = setInterval(function(){
+          imageIndex++;
+          slide.src = newThumbs[imageIndex];
+        }, 3000);
+      });
+    hideContent();
+    
+    });
+
+    stopBttn.addEventListener("click", function() {
+      showContent();
+      clearInterval(handle);
+    });
+})
+
+function showContent() {
+  if (description.style.display === "none" && textInput.style.display === "none") {
+    description.style.visibility = "visible";
+}};
+
+function hideContent() {
+  description.style.display = "none";
+  textInput.style.display = "none";
+  //document.getElementById("show").style.display = "none";
+}
 
 //Event listener for Play button
+/*
 playBttn.addEventListener("click", function(e) {
   var wordInput = textInput.value;
   searchWord = String.fromCharCode(e.which).toLowerCase();
@@ -37,7 +93,7 @@ playBttn.addEventListener("click", function(e) {
 
         //document.getElementById("slides").src = jsonData.data.children[i].data.thumbnail;
         //console.log(jsonData.data.children[1].data.thumbmail);
-
+*/
         /*
         var intervalHandle = setInterval(function() {
           for (var j = 0; j < 25; j++) {
@@ -47,10 +103,11 @@ playBttn.addEventListener("click", function(e) {
       }
       */
         //images.forEach(function(item) {
-    }
+    //}
     
-    })
-    hideContent();
+    //})
+    //hideContent();
+    //playSlides();
     /*
     textInput.addEventListener("click", function(e) {
       e.preventDefault()
@@ -58,19 +115,35 @@ playBttn.addEventListener("click", function(e) {
     */
     
   
-});
+//});
 
-//Event listener for stop button:
-stopBttn.addEventListener("click", function(e) {
-  showContent();
-});
-
-function hideContent() {
-  description.style.display = "none";
-  textInput.style.display = "none";
+/*
+function playSlides() {
+  for (var i = 0; i < imagesArray.length; i++) {
+    document.getElementById("slides").src = imagesArray[i];
+    //document.getElementById("slides").src = jsonData.data.children[i].data.thumbnail;
+  }
+    intervalHandle = setInterval(playSlides, 2000);
+  
 }
+*/
 
-function showContent() {
-  if (description.style.display === "none" && textInput.style.display === "none") {
-    description.style.visibility = "visible";
-}};
+/*
+//Event listener for stop button:
+stopBttn.addEventListener("click", function() {
+  showContent();
+  clearInterval(intervalHandle);
+});*/
+
+/*
+stopBttn.onclick = function() {
+  showContent();
+}
+*/
+
+
+
+ 
+
+
+
