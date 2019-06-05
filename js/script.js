aa8adc456e066226e20659c8c7d2984f4ff54fda
Kelsey Cox <kelseycox@Lucy.local>
2019-06-05 12:12:42 -0700

    coded along and added a cat

--- js/script.js
+++ js/script.js
@@ -5,34 +5,59 @@
 // var pictureEL = document.getElementById("picture");
 
 var submitBtn;
+var stopBtn;
 var inputEl;
 var pictureEl;
+var slidesEl;
 var splash;
 var searchTerm = '';
 var searchUrl1 = 'https://www.reddit.com/search.json?q='
 var searchUrl2 = '+nsfw:no';
 var url = '';
-
+var imageIndex = 0;
+var handle = null;
 
 document.addEventListener('DOMContentLoaded', function(){
     inputEl = document.querySelector('[name="search"]');
     submitBtn = document.getElementById("submit");
-    pictureEL = document.getElementById("picture")
-    splash = document.getElementById("splash")
-    
+    pictureEl = document.getElementById("picture");
+    splash = document.getElementById("splash");
+    stopBtn = document.getElementById("stop");
+
+    stopBtn.addEventListener('click', function(e) {
+        clearInterval(handle);
+        splash.style.visibility = "visible";
+        stopBtn.style.visibility = "hidden";
+        pictureEl.src = "https://placekitten.com/200/200"
+    })
+
     submitBtn.addEventListener("click", function(e) {    
         searchTerm = inputEl.value;
         inputEl.value = '';
+        splash.style.visibility = "hidden";
+        stopBtn.style.visibility = "visible";
         url = searchUrl1 + searchTerm + searchUrl2;
         fetch(url)
             .then(function(data) {
                 return data.json();
             })
             .then(function(json) {
-                var posts = json.data.children[1].data.thumbnail;
-                console.log(posts);
-                pictureEl.src = json.data.children[1].data.thumbnail
-        });
+                console.log(json.data.children[1].data.thumbnail);
+                var newThumbs = json.data.children.map(function(thumb) {
+                    return thumb.data.thumbnail;
+                })
+                pictureEl.src = newThumbs[0];
+                handle = setInterval(function() {
+                    if (imageIndex >= newThumbs.length) {
+                        imageIndex = -1
+                    }
+                    imageIndex++;
+                    pictureEl.src = newThumbs[imageIndex];
+                }, 1000);
+            });
+    stopBtn.addEventListener("click", function (e) {
+
+    })        
     });
 });
 
