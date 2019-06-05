76f31914f4e592f8c1c3db0ecd28c829cb22943c
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-05 13:19:45 -0700

    added hide/show description functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 
-var values = [];
-var imagesArray = [];
-var slideIndex = 0;
 var redditUrlFirst = "https://www.reddit.com/search.json?q=";
 var redditUrlLast = "+nsfw:no";
 var url = "";
-var searchWordArray = [];
 var searchWord = "";
-var clearInterval;
 var wordInput = "";
 var imageIndex = 0;
 var handle = null;
@@ -24,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
   stopBttn = document.getElementById("stop");
   textInput = document.getElementById("text");
   slide = document.getElementById("slides");
+
+  slide.style.display="none";
 
   playBttn.addEventListener("click", function(e) {
     wordInput = textInput.value;
@@ -42,108 +39,30 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         slide.src = newThumbs[0];
         handle = setInterval(function(){
+          //can also use if(imageIndex >= newThumbs.length)
+          if (imageIndex > 24) { 
+            imageIndex = -1;
+          }
           imageIndex++;
           slide.src = newThumbs[imageIndex];
-        }, 3000);
+        }, 800);
+      
       });
     hideContent();
+    slide.style.display = "inline-block";
     
     });
 
-    stopBttn.addEventListener("click", function() {
-      showContent();
-      clearInterval(handle);
+  stopBttn.addEventListener("click", function(e) {
+    description.style.display = "block";
+    textInput.style.display = "inline-block";
+    slide.style.display = "none";
+    clearInterval(handle);
     });
-})
-
-function showContent() {
-  if (description.style.display === "none" && textInput.style.display === "none") {
-    description.style.visibility = "visible";
-}};
+});
 
 function hideContent() {
   description.style.display = "none";
   textInput.style.display = "none";
-  //document.getElementById("show").style.display = "none";
 }
-
-//Event listener for Play button
-/*
-playBttn.addEventListener("click", function(e) {
-  var wordInput = textInput.value;
-  searchWord = String.fromCharCode(e.which).toLowerCase();
-  textInput.value = ""; 
-  searchWordArray = searchWordArray + wordInput;
-  redditUrl = redditUrl + searchWordArray;
-  
-  console.log(searchWordArray);
-  console.log(redditUrl);
-  
-  fetch(redditUrl) 
-    .then(function(responseData) {
-      return responseData.json();
-    })
-    .then(function(jsonData) {
-      for (var i = 0; i < 1000; i++) {
-        values = jsonData.data.children[i].data.thumbnail;
-        document.getElementById("slides").src = jsonData.data.children[i].data.thumbnail;
-        //console.log(values);
-        imagesArray.push(values);
-        console.log(imagesArray);
-
-        //document.getElementById("slides").src = jsonData.data.children[i].data.thumbnail;
-        //console.log(jsonData.data.children[1].data.thumbmail);
-*/
-        /*
-        var intervalHandle = setInterval(function() {
-          for (var j = 0; j < 25; j++) {
-            document.getElementById("slides").src = jsonData.data.children[j].data.thumbnail;
-          }
-        }, 5000);
-      }
-      */
-        //images.forEach(function(item) {
-    //}
-    
-    //})
-    //hideContent();
-    //playSlides();
-    /*
-    textInput.addEventListener("click", function(e) {
-      e.preventDefault()
-    });
-    */
-    
-  
-//});
-
-/*
-function playSlides() {
-  for (var i = 0; i < imagesArray.length; i++) {
-    document.getElementById("slides").src = imagesArray[i];
-    //document.getElementById("slides").src = jsonData.data.children[i].data.thumbnail;
-  }
-    intervalHandle = setInterval(playSlides, 2000);
-  
-}
-*/
-
-/*
-//Event listener for stop button:
-stopBttn.addEventListener("click", function() {
-  showContent();
-  clearInterval(intervalHandle);
-});*/
-
-/*
-stopBttn.onclick = function() {
-  showContent();
-}
-*/
-
-
-
- 
-
-
 
