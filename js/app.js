ddb6fa1192224a996567904ba20650f6d1faea99
Pete Fitton <petefitton@Pete-Fittons-MacBook.local>
2020-01-30 08:36:46 -0800

    final updates

--- js/app.js
+++ js/app.js
@@ -3,74 +3,50 @@ let arr = [];
 let resetButtonEnable = function() {
     document.querySelector(".button").value = "Reset";
     document.querySelector(".inputBox").hidden = true;
-    // should add event listener for resetting the page effectively
+    document.querySelector("h2").hidden = true;
+    document.querySelector('img').src = "";
+    document.querySelector("img").hidden = false;
+    // add event listener for resetting the page effectively
     document.querySelector(".button").addEventListener('click', resetButtonDisable);
     document.querySelector('.button').removeEventListener('click', fetchFun);
 };
 
 let resetButtonDisable = function() {
     document.querySelector(".inputBox").hidden = false;
+    document.querySelector("h2").hidden = false;
+    document.querySelector("img").hidden = true;
     document.querySelector('.button').removeEventListener('click', resetButtonDisable);
     document.querySelector(".button").value = "Submit your wish";
     document.querySelector('.button').addEventListener('click', fetchFun);
 };
 
-// let displayFunction = function() {
+let displayFunction = function() {
 
-// setTimeout(function() {
+    setTimeout(function() {
 
-
-
-
-//     console.log("displayFunction running")
-//     // want to place img in the imgField
-//     // document.querySelector('.imgField').appendChild(imgReddit);
-
-//     for (i = 0; i < arr.length; i++) {
-//         setTimeout(function() {
-//             //empty
-//             let imgField = document.querySelector('.imgField');
-//             //these two directly below work but for some reason the arr[i] and arr[i + 1] is not working; seems to be NODE related
-//             // imgField.removeChild(arr[0]);
-//             // imgField.appendChild(arr[1]);
-//             // let tempImg= document.createElement('img');
-//             // tempImg = arr[i]
-//             imgField.removeChild(imgField.childNodes[i]);
-//             //insert one pic into
-//             // tempImg2 = arr[i+1]
-//             imgField.appendChild(imgField.childNodes[i + 1]);
-//         }, 2000);
-//     };
-// }, 2000);
-// }
-
-
-// deetsINeed.forEach(function(oneResult) {
-//     let listItem = document.createElement('li');
-//     // create an li elem
-//     let title = document.createElement('h3');
-//     // create h3
-//     title.textContent = oneResult.title;
-//     // add the h3 title to li
-//     listItem.appendChild(title);
-//     // create an a tag
-//     let link = document.createElement('a');
-//     // set a tag href
-//     link.href = "https://www.reddit.com/" + oneResult.url
-//     link.textContent = 'See it on Reddit!'
-//     // add the a tag with url to li
-//     listItem.appendChild(link);
-//     // append the li elem to parent
-//     redditResultsDOM.appendChild(listItem);
-    
+        console.log("displayFunction running")
+        // want to change src of the img in the HTML file with the urls I stored in arr[]
+        
+        let i = 0;
+        document.querySelector('img').src = arr[i];
+            
+        setInterval(function() {
+            // if more time, would troubleshoot which images don't appear and why and skip over them when displaying
+            i++;
+            if (i > arr.length) {
+                i = 0;
+            }
+            document.querySelector('img').src = arr[i];
+            console.log(arr[i]);
+            console.log("running setInterval function")
+        // changing this number will change how fast the pictures change
+        }, 2000);
+    }, 1500);
+}
 
 let fetchFun = function() {
     arr = [];
     console.log("Testz");
-
-    // document.querySelector('.button').addEventListener('click', function() {
-        
-    console.log("Testy");
 
     fetch(`https://www.reddit.com/search.json?q=${document.querySelector(".inputBox").value}`)
     .then(function(responseData) {
@@ -87,107 +63,23 @@ let fetchFun = function() {
             let oneResult = redditResult.data.url;
             console.log(oneResult);
             return oneResult;
-            // return { title: String, url: String }
         });
         
-        // get parent elem
-        let redditResultsDOM = document.querySelector('#reddit-results');
         // iterate over my list
         neededInfo.forEach(function(oneResult) {
-            // setTimeout(function(){
 
-                    let imgReddit = document.createElement('img');
-                    imgReddit.src = oneResult;
-                    arr.push(imgReddit);
-                    // create an li elem
-                    // let title = document.querySelector('.imgField');
-                    // create h3
-                    // title.textContent = oneResult.title;
-                    // add the h3 title to li
-
-
-
-
-
-                    let listItem = document.createElement('li');
-                    // create an li elem
-                        // let title = document.createElement('h3');
-                    // create h3
-                        // title.textContent = oneResult.title;
-                    // add the h3 title to li
-                    listItem.appendChild(imgReddit);
-                    // create an a tag
-                    // append the li elem to parent
-                    
-                    let displayFunction = function() {
-
-                        setTimeout(function() {
-                        
-                        
-                        
-                        
-                            console.log("displayFunction running")
-                            // want to place img in the imgField
-                            // document.querySelector('.imgField').appendChild(imgReddit);
-                        
-                            // for (i = 0; i < arr.length; i++) {
-                                setTimeout(function() {
-                                    //empty
-                                    let imgField = document.querySelector('.imgField');
-                                    imgField.removeChild(imgField.childNodes[0]);
-                                    imgField.appendChild(listItem);
-                                // let imgField = document.querySelector('.imgField');
-                                    //these two directly below work but for some reason the arr[i] and arr[i + 1] is not working; seems to be NODE related
-                                    // imgField.removeChild(arr[0]);
-                                    // imgField.appendChild(arr[1]);
-                                    // let tempImg= document.createElement('img');
-                                    // tempImg = arr[i]
-                                // imgField.removeChild(imgField.childNodes[i]);
-                                    //insert one pic into
-                                    // tempImg2 = arr[i+1]
-                                // imgField.appendChild(imgField.childNodes[i + 1]);
-                                }, 2000);
-                            // };
-                        }, 4000);
-                    }
-                    
-                    
-                    
-                    displayFunction();
-                    
-                    // want to place img in the imgField
-                    // title.appendChild(imgReddit);
-                    
-                    // }, 2500);
-                    // setTimeout(function() {
-                        
-                        //     document.querySelector("img").remove();
-                        // }, 10);
-                        
-                        
-                        
-                        // create an a tag
-                        // let link = document.createElement('a');
-                        // set a tag href
-                        // link.href = "https://www.reddit.com/" + oneResult.url
-                        // link.textContent = 'See it on Reddit!'
-                        // add the a tag with url to li
-                        // imgReddit.appendChild(link);
-                        // append the li elem to parent
-                        // redditResultsDOM.appendChild(imgReddit);
-                    });
-                    console.log(arr);
-                    // document.querySelector('.imgField').appendChild(arr[0]);
-
-
-                            // call display function
-                            // $('.imgField').slick({
-                                //     infinite: true,
-                                //     slidesToShow: 3,
-                //     slidesToScroll: 3
-        });   
-        resetButtonEnable();
+            arr.push(oneResult);            
+            
+        });
         
+        // run displayFunction()
+        displayFunction();
+        console.log(arr);
+
+    });
+
+        resetButtonEnable();
+
 };
 
 
@@ -211,24 +103,3 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.button').addEventListener('click', fetchFun);
     
 });
-
-
-
-
-
-
-
-// neededInfo.forEach(function(oneResult) {
-//     // setTimeout(function(){
-
-//             let imgReddit = document.createElement('img');
-//             imgReddit.src = oneResult;
-//             // create an li elem
-//             let title = document.querySelector('.imgField');
-//             // create h3
-//             // title.textContent = oneResult.title;
-//             // add the h3 title to li
-            
-//             arr.push(imgReddit);
-            
-//         });
